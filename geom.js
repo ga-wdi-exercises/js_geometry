ba0aa9e0af1cd605a308e358ca9db44225a286e6
Clarissa Stark <clarissa.stark@gmail.com>
2016-02-27 17:29:12 -0500

    rectangle hw complete

--- geom.js
+++ geom.js
@@ -3,6 +3,23 @@ function Rectangle(length, width) {
   this.width = width;
 }
 
+Rectangle.prototype.isSquare = function(){
+  if(this.width == this.length){
+    return true;
+  }else{
+    return false;
+  }
+};
+
+Rectangle.prototype.area = function(){
+  return this.length * this.width;
+};
+
+Rectangle.prototype.perimeter = function(){
+  return this.length*2 + this.width*2;
+};
+
+// var skinnyBox = new Rectangle(18,2);
 
 function Triangle(sideA, sideB, sideC) {
   this.sideA = sideA;
@@ -10,6 +27,15 @@ function Triangle(sideA, sideB, sideC) {
   this.sideC = sideC;
 }
 
+$("h2").on("click",function(){
+  console.log($(this).html())
+}
+);
+
+$("h2").click(function(){
+  alert(this.innerHTML);
+}
+);
 
 function LineSegment(x1, y1, x2, y2) {
   this.x1 = x1;
