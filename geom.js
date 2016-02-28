ea009078327dbd9e89d101e74f0076d405929d82
Melissa Miller <lis.d.miller@gmail.com>
2016-02-28 08:24:22 -0500

    Adds methods and tests for Triangle, LineSegment
    Creates isEquilateral, isIsosceles, area, and isObtuse for Triangle.
    Creates length for LineSegment.

--- geom.js
+++ geom.js
@@ -22,9 +22,26 @@ function Triangle(sideA, sideB, sideC) {
 }
 
 Triangle.prototype.isEquilateral = function () {
-
+  return (this.sideA == this.sideB) && (this.sideA == this.sideC);
 };
 
+Triangle.prototype.isIsosceles = function () {
+  return (this.sideA == this.sideB) || (this.sideA == this.sideC) || (this.sideB == this.sideC);
+};
+
+Triangle.prototype.area = function () {
+  var half_perimeter = (this.sideA + this.sideB + this.sideC)/2;
+  var area_squared = half_perimeter * (half_perimeter - this.sideA) * (half_perimeter - this.sideB) * (half_perimeter - this.sideC);
+
+  return Math.sqrt(area_squared);
+};
+
+Triangle.prototype.isObtuse = function () {
+  var sides = [this.sideA,this.sideB,this.sideC];
+  sides.sort();
+
+  return (Math.pow(sides[0], 2) + Math.pow(sides[1], 2)) < Math.pow(sides[2], 2);
+};
 
 function LineSegment(x1, y1, x2, y2) {
   this.x1 = x1;
@@ -33,4 +50,13 @@ function LineSegment(x1, y1, x2, y2) {
   this.y2 = y2;
 }
 
+LineSegment.prototype.length = function () {
+  var diffX = this.x2 - this.x1;
+  var diffY = this.y2 - this.y1;
+
+  return Math.sqrt(diffX * diffX + diffY * diffY);
+};
+
 module.exports.Rectangle = Rectangle;
+module.exports.Triangle = Triangle;
+module.exports.LineSegment = LineSegment;
