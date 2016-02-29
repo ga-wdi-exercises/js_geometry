09ff6d394e1d7f3f32d672d70824aaf64feacf54
Toni Langley <chibia2@gmail.com>
2016-02-29 18:56:13 -0500

    Line segment method: done

--- geom.js
+++ geom.js
@@ -1,3 +1,4 @@
+//RECTANGLE
 function Rectangle(length, width) {
   this.length = length;
   this.width = width;
@@ -11,21 +12,22 @@ Rectangle.prototype.isSquare = function(){
   else{
     return false;
   }
-}
+};
 
 //`area` - calculates the area of the rectangle.
 Rectangle.prototype.area = function(){
   return this.width * this.length;
-}
+};
 
 //`perimeter` - calculates the perimeter of the rectangle.
 Rectangle.prototype.perimeter = function(){
   return 2*(this.length + this.width);
-}
+};
 
 var square = new Rectangle(2, 2);
 var notASquare = new Rectangle(3, 4);
 
+//TRIANGLE
 function Triangle(sideA, sideB, sideC) {
   this.sideA = sideA;
   this.sideB = sideB;
@@ -33,9 +35,20 @@ function Triangle(sideA, sideB, sideC) {
 }
 
 
+//LINE SEGEMENT
 function LineSegment(x1, y1, x2, y2) {
   this.x1 = x1;
   this.y1 = y1;
   this.x2 = x2;
   this.y2 = y2;
 }
+
+//`length` – calculates the length of the (x1, y1) --> (x2, y2) [line segment]
+LineSegment.prototype.length = function(){
+  //a^2 + b^2 = c^2
+  var ansX =  Math.sqrt((this.x1 * this.x1) + (this.y1 * this.y1));
+
+  var ansY = Math.sqrt((this.x2 * this.x2) + (this.y2 * this.y2));
+
+  return "(" + Math.round(Math.ceil(ansX)) + ", " + Math.round(Math.ceil(ansY)) + ")";
+};
