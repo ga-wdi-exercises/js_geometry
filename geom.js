3a679cda1b587d23babb4f04572d3f27cd3e4bb1
Adrian Wyatt <Adi.Wyatt@outlook.com>
2016-02-28 19:19:50 -0500

    inital commit, finished rectangle part

--- geom.js
+++ geom.js
@@ -1,7 +1,23 @@
+
+
 function Rectangle(length, width) {
   this.length = length;
   this.width = width;
 }
+Rectangle.prototype.IsSquare = function(){
+  if (this.length === this.width){
+    return "I'm a square!"
+  }else{return "Not a square, just a rectangle"}
+}
+
+Rectangle.prototype.area = function(){
+  return "My area is "+ (this.length * this.width);
+}
+Rectangle.prototype.perimeter = function(){
+  return "My perimeter is "+(2*(this.length+this.width))
+}
+var square = new Rectangle(4,4);
+var notSquare = new Rectangle(4,6);
 
 
 function Triangle(sideA, sideB, sideC) {
@@ -9,6 +25,7 @@ function Triangle(sideA, sideB, sideC) {
   this.sideB = sideB;
   this.sideC = sideC;
 }
+
 
 
 function LineSegment(x1, y1, x2, y2) {
