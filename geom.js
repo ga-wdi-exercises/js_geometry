4898134b72163cae16f2f391207587d620c73c53
Toni Langley <chibia2@gmail.com>
2016-02-29 17:16:52 -0500

    Rectangle methods: done

--- geom.js
+++ geom.js
@@ -3,6 +3,28 @@ function Rectangle(length, width) {
   this.width = width;
 }
 
+//`isSquare` - returns true if the rectangle is a square.
+Rectangle.prototype.isSquare = function(){
+  if(this.length == this.width){
+    return true;
+  }
+  else{
+    return false;
+  }
+}
+
+//`area` - calculates the area of the rectangle.
+Rectangle.prototype.area = function(){
+  return this.width * this.length;
+}
+
+//`perimeter` - calculates the perimeter of the rectangle.
+Rectangle.prototype.perimeter = function(){
+  return 2*(this.length + this.width);
+}
+
+var square = new Rectangle(2, 2);
+var notASquare = new Rectangle(3, 4);
 
 function Triangle(sideA, sideB, sideC) {
   this.sideA = sideA;
