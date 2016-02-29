9dfae2571efd0546ced56a99f7f6d79c5dcf340c
Matthew Stevens <matthew.lee.stevens@gmail.com>
2016-02-28 19:18:16 -0500

    finished line segment

--- geom.js
+++ geom.js
@@ -68,3 +68,7 @@ function LineSegment(x1, y1, x2, y2) {
   this.x2 = x2;
   this.y2 = y2;
 }
+
+LineSegment.prototype.length = function() {
+    return Math.sqrt((Math.pow((this.y2-this.y1), 2))+(Math.pow((this.x2-this.x1), 2)));
+};
