f7ceb1b453b0c385bd07c79d0edbac8f07d8b53a
Christine Movius <christine.movius@gmail.com>
2016-02-29 13:26:19 -0500

    create isSquare and area methods.

--- geom.js
+++ geom.js
@@ -1,15 +1,23 @@
+var rect1 = new Rectangle(3,3);
+var rect2 = new Rectangle(4,3);
+var rect3 = new Rectangle(3,5);
+
 function Rectangle(length, width) {
   this.length = length;
   this.width = width;
 }
 Rectangle.prototype.isSquare = function() {
-
+  if (this.length === this.width) {
+    return true;
+  } else {
+    return false;
+  }
 };
 Rectangle.prototype.area = function() {
-
+  return (this.length * this.width);
 };
 Rectangle.prototype.perimeter = function() {
-
+  //return ((2 * this.length) + (2 * this.height));
 };
 
 
