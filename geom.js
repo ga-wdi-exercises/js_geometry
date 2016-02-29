6092d0ac9c6b66eae0a92a5c8f0cb04b7b5c2b65
Vijender Singh Bajwa <vbajwamd@gmail.com>
2016-02-28 20:02:04 -0500

    initial commit

--- geom.js
+++ geom.js
@@ -3,6 +3,17 @@ function Rectangle(length, width) {
   this.width = width;
 }
 
+Rectangle.prototype = {
+  isSquare: function(){
+    return this.length === this.width;
+  },
+  area: function(){
+    return this.length * this.width;
+  },
+  perimeter: function(){
+    return (2*this.length) + (2*this.width);
+  }
+};
 
 function Triangle(sideA, sideB, sideC) {
   this.sideA = sideA;
@@ -10,6 +21,25 @@ function Triangle(sideA, sideB, sideC) {
   this.sideC = sideC;
 }
 
+Triangle.prototype = {
+  isEquilateral: function(){
+    return this.sideA === this.sideB && this.sideB === this.sideC;
+  },
+  Isosceles: function(){
+    return this.sideA === this.sideB || this.sideA === this.sideC || this.sideB === this.sideC;
+  },
+  Area: function(){ //hmm its 1/2B*H found this online: var perimeter = (side1 + side2 + side3)/2;
+//var area =  Math.sqrt(perimeter*((perimeter-side1)*(perimeter-side2)*(perimeter-side3)));
+//console.log(area);
+    //if
+    var p = (this.sideA + this.sideB + this.sideC)/2;
+    var area = Math.sqrt(p * (p - this.sideA) * (p - this.sideB) * (p - this.sideC));
+    return area;
+  },
+  isObtuse: function(){
+    //I HVE NOIDEA AND THE SOLUTION DOESNT EVEN MAKE SENSE. 
+  }
+}
 
 function LineSegment(x1, y1, x2, y2) {
   this.x1 = x1;
