53eb9797c7423a48f796826e8d5fa4f1f0dd3213
Charles <charlesthwang@gmail.com>
2016-02-29 14:12:09 -0500

    triangle and linesegment prototypes

--- geom.js
+++ geom.js
@@ -15,11 +15,34 @@ Rectangle.prototype.perimeter = function() {
   return (this.length * 2) + (this.width * 2);
 };
 
+
+
 function Triangle(sideA, sideB, sideC) {
   this.sideA = sideA;
   this.sideB = sideB;
   this.sideC = sideC;
 }
+
+Triangle.prototype.isEquilateral = function() {
+  if (this.sideA === this.sideB === this.sideC) {
+    return "True";
+  }
+};
+Triangle.prototype.isIsoceles = function() {
+  if (this.sideA !== this.sideB !== this.sideC) {
+    return "True";
+  }
+};
+Triangle.prototype.area = function() {
+  var s = (this.sideA + this.sideB + this.sideC)/2;
+  return Math.sqrt((s)*(s - this.sideA)*(s - this.sideB)*(s - this.sideC));
+};
+Triangle.prototype.isObtuse = function() {
+  if (this.sideA === this.sideB || this.sideA === this.sideC || this.sideB === this.sideC) {
+    return "True";
+  }
+};
+
 
 
 function LineSegment(x1, y1, x2, y2) {
@@ -28,3 +51,7 @@ function LineSegment(x1, y1, x2, y2) {
   this.x2 = x2;
   this.y2 = y2;
 }
+
+LineSegment.prototype.length = function() {
+  return Math.sqrt(Math.pow((this.x2 - this.x1), 2) + Math.pow((this.y2 - this.y1), 2));
+};
