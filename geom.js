b74c0545b0c43119664abd0498a0cb012766aa24
Christine Movius <christine.movius@gmail.com>
2016-02-29 17:07:01 -0500

    complete rectangle isSquare, area, perimeter.

--- geom.js
+++ geom.js
@@ -3,8 +3,8 @@ var rect2 = new Rectangle(4,3);
 var rect3 = new Rectangle(3,5);
 
 function Rectangle(length, width) {
-  this.length = length;
-  this.width = width;
+  this.length = parseFloat(length);
+  this.width = parseFloat(width);
 }
 Rectangle.prototype.isSquare = function() {
   if (this.length === this.width) {
@@ -17,7 +17,9 @@ Rectangle.prototype.area = function() {
   return (this.length * this.width);
 };
 Rectangle.prototype.perimeter = function() {
-  //return ((2 * this.length) + (2 * this.height));
+  var lengthSum = 2 * this.length;
+  var widthSum = 2 * this.width;
+  return lengthSum + widthSum;
 };
 
 
