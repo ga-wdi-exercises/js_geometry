95e57b450c494b556276c548301a9e62d4079cbe
Adrian Wyatt <Adi.Wyatt@outlook.com>
2016-02-28 20:30:39 -0500

    did the line section and half the triangle stuff

--- geom.js
+++ geom.js
@@ -26,11 +26,53 @@ function Triangle(sideA, sideB, sideC) {
   this.sideC = sideC;
 }
 
+Triangle.prototype.isEquilateral = function(){
+  if(this.sideA === this.sideB){
+    if (this.sideB===this.sideC){
+      return "I'm equilateral"
+    }else if (this.sideB!==this.sideC){
+      return "I'm isoceles! Run me under that prototype instead!"
+    }
+  }else{return "I'm not equilateral!"}
+}
+//I know I can clean this up but I'm not sure how just yet. I'm just thrilled it works!
+Triangle.prototype.isIsosceles = function(){
+    if(this.sideA===this.sideB){
+    if (this.sideB !== this.sideC){
+        return "I'm Isoceles!"
+      }else{return "I'm Equilateral. There's a prototype to prove it!"}
+    }else if (this.sideA===this.sideC){
+      return "I'm Isoceles too!"
+    }else{
+      return "I'm neither isoceles nor am I equilateral"
+    }
+}
+// do this for the area - Yall? Seriously? What's with the math? Man.... hahaha
+Triangle.prototype.area = function(){
 
+}
+Triangle.prototype.isObtuse = function(){
 
+}
+
+var equilateralTriangle = new Triangle(5,5,5);
+var isoscelesTriangle = new Triangle(5,5,3);
+var alsoNotEquilateral = new Triangle(5,2,4);
+var anotherIsoscelesTriangle = new Triangle(4,3,4)
+//==============================================================//
 function LineSegment(x1, y1, x2, y2) {
   this.x1 = x1;
   this.y1 = y1;
   this.x2 = x2;
   this.y2 = y2;
 }
+
+///this is my proudest moment in code so far. I found this out all on my own. I mean...with google and our lessons but not by any other help. It works! 
+LineSegment.prototype.length = function(){
+  this.Xdiff = ((this.x2)-(this.x1));
+  this.Ydiff = ((this.y2-this.y1));
+  this.distance = Math.sqrt(Math.pow(this.Xdiff,2)+Math.pow(this.Ydiff,2))
+  return "I'm "+ this.distance + " long."
+}
+
+var lineyLineLine = new LineSegment(4,3,5,7)
