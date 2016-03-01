1e31d92828579c1fd351e78c59e648cb544c81d3
Keith Prifte <kprift@gmail.com>
2016-03-01 18:58:23 -0500

    adds triangle prototypes

--- geom.js
+++ geom.js
@@ -14,7 +14,7 @@ Rectangle.prototype.area = function() {
 }
 
 Rectangle.prototype.perimeter = function() {
-  return (this.width * 2) + (this.length * 2); 
+  return (this.width * 2) + (this.length * 2);
 }
 
 function Triangle(sideA, sideB, sideC) {
@@ -23,6 +23,32 @@ function Triangle(sideA, sideB, sideC) {
   this.sideC = sideC;
 }
 
+Triangle.prototype.isEquilateral = function() {
+  if ((this.sideA === this.sideB) && (this.sideA === this.sideC) && (this.sideB === this.sideC)) {
+    return true;
+  } else return false;
+}
+
+Triangle.prototype.isIsosceles = function() {
+  if ((this.sideA === this.sideB) && (this.sideA !== this.sideC)) {
+    return true;
+  } else if ((this.sideB === this.sideC) && (this.sideB !== this.sideA)) {
+    return true;
+  } else if ((this.sideA === this.sideC) && (this.sideA !== this.sideB)) {
+    return true;
+  } else return false
+}
+
+Triangle.prototype.area = function() {
+  area = ((.5) * (this.sideA * this.sideC))
+
+  return area;
+
+}
+
+// Triangle.prototype.isObtuse = function() {
+//
+// }
 
 function LineSegment(x1, y1, x2, y2) {
   this.x1 = x1;
