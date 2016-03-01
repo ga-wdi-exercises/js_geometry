c8f355c262d13c87178b51c83a8358a47d47e30a
Craig Eisen <Ceisen91@gmail.com>
2016-03-01 13:55:18 -0500

    second commit

--- geom.js
+++ geom.js
@@ -19,9 +19,9 @@ Rectangle.prototype.perimeter = function(){
   return this.length * 2 + this.width * 2;
 };
 
-var rectangle1 = new Rectangle(4, 5);
-var rectangle2 = new Rectangle(3, 3);
-var rectangle3 = new Rectangle(10, 9);
+// var rectangle1 = new Rectangle(4, 5);
+// var rectangle2 = new Rectangle(3, 3);
+// var rectangle3 = new Rectangle(10, 9);
 
 function Triangle(sideA, sideB, sideC) {
   this.sideA = sideA;
@@ -58,11 +58,11 @@ Triangle.prototype.isObtuse= function(){
   }
 };
 
-var triangle1 = new Triangle(3,4,5);
-var triangle2 = new Triangle(3,4,9);
-var triangle3 = new Triangle(3,4,4.5);
-var triangle4 = new Triangle(3,3,4.5);
-var triangle5 = new Triangle(3,3,3);
+// var triangle1 = new Triangle(3,4,5);
+// var triangle2 = new Triangle(3,4,9);
+// var triangle3 = new Triangle(3,4,4.5);
+// var triangle4 = new Triangle(3,3,4.5);
+// var triangle5 = new Triangle(3,3,3);
 
 
 function LineSegment(x1, y1, x2, y2) {
@@ -76,7 +76,7 @@ LineSegment.prototype.length = function(){
   return Math.sqrt(Math.pow(Math.abs(this.x1-this.x2),2) + Math.pow(Math.abs(this.y1-this.y2),2) );
 };
 
-var lineSegment1 = new LineSegment(1,1,1,1);
-var lineSegment2 = new LineSegment(0,0,3,4);
-var lineSegment3 = new LineSegment(0,0,-4, -3);
-var lineSegment4 = new LineSegment(1,1,7,9);
+// var lineSegment1 = new LineSegment(1,1,1,1);
+// var lineSegment2 = new LineSegment(0,0,3,4);
+// var lineSegment3 = new LineSegment(0,0,-4, -3);
+// var lineSegment4 = new LineSegment(1,1,7,9);
