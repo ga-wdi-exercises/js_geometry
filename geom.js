f4d064a81979cb22d3dd09767327a1adcf05655d
Ryan Krolick <rkrolick@gmail.com>
2016-03-01 18:40:46 -0500

    added requested methods for triangle object

--- geom.js
+++ geom.js
@@ -1,3 +1,8 @@
+var rects = [new Rectangle(2,9), new Rectangle(300,550), new Rectangle(60,60)];
+var tris = [new Triangle(6,6,6), new Triangle(300,550,610), new Triangle(12,10,3)];
+
+
+
 function Rectangle(length, width) {
   this.length = length;
   this.width = width;
@@ -22,6 +27,25 @@ function Triangle(sideA, sideB, sideC) {
   this.sideC = sideC;
 }
 
+Triangle.prototype.isEquilateral = function(){
+  return (this.sideA == this.sideB == this.sideC);
+}
+
+Triangle.prototype.isIsoceles = function(){
+  return ((this.sideA == this.sideB) || (this.sideA == this.sideC) || (this.sideB == this.sideC));
+}
+
+Triangle.prototype.area = function(){
+  var s = ((this.sideA+this.sideB+this.sideC)/2)
+  return Math.sqrt(s*(s-this.sideA)*(s-this.sideB)*(s-this.sideC));
+}
+
+Triangle.prototype.isObtuse = function(){
+  var c = Math.max(this.sideA, this.sideB, this.sideC);
+  if ( c == this.sideA){return Math.pow(this.sideA, 2 ) > (Math.pow(this.sideB,2) + Math.pow(this.sideC,2)); }
+  else if ( c == this.sideB){return Math.pow(this.sideB, 2 ) > (Math.pow(this.sideA,2) + Math.pow(this.sideC,2)); }
+  else {return Math.pow(this.sideC, 2 ) > (Math.pow(this.sideA,2) + Math.pow(this.sideB,2)); }
+}
 
 function LineSegment(x1, y1, x2, y2) {
   this.x1 = x1;
