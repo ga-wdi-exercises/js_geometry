9d43f24ffb06424ea1edb54c7283ccce2bb782b2
Kimberly Cabbagestalk <kimcabb@gmail.com>
2016-03-02 09:54:41 -0500

    changes made

--- geometry.js
+++ geometry.js
@@ -1,35 +1,18 @@
-function Rectangle(length, width) {
-  this.length = length;
-  this.width = width;
-}
-
-/*Rectangle.prototype.isSquare = function(){
+Rectangle.prototype.isSquare = function(){
   if (length === width) {
     return true;
+    }
   }
-}
-*/
+
 Rectangle.prototype.area = function(){
   var area = (length * width);
   return area;
-  console.log(area);
   }
 
 Rectangle.prototype.perimeter = function(){
-  var perimeter = ((length*2)+(width*2));
+ var perimeter = ((length*2)+(width*2));
   return perimeter;
   }
-
-var rectOne = new Rectangle(7,3);
-var rectTwo = new Rectangle(4,5);
-var rectThree = new Rectangle(3,3);
-
-/*
-function Triangle(sideA, sideB, sideC) {
-  this.sideA = sideA;
-  this.sideB = sideB;
-  this.sideC = sideC;
-}
 
 Triangle.prototype.area = function(){
   //calculate height using Heron's formula
@@ -37,14 +20,6 @@ Triangle.prototype.area = function(){
   return ess;
   var area = sqr(ess((ess-sideA)*(ess-sideB)*(ess-sideC)));
   return area;
-}
-
-function LineSegment(x1, y1, x2, y2) {
-  this.x1 = x1;
-  this.y1 = y1;
-  this.x2 = x2;
-  this.y2 = y2;
-}
 
 LineSegment.prototype.length = function(){
   //var length = function is hypotenuse...
@@ -53,5 +28,4 @@ LineSegment.prototype.length = function(){
   //use Pythagorean theorem to calculate hypotenuse
   var hyp = sqr((height*height)+(base*base));
   return hyp;
-}
-*/
+  
