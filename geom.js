642caf4d7cdebc25e77a4309064b58e7a4eedd94
Fatai Agiri <yinka.fatai@gmail.com>
2016-03-01 23:51:35 -0500

    solved rectangle

--- geom.js
+++ geom.js
@@ -3,6 +3,27 @@ function Rectangle(length, width) {
   this.width = width;
 }
 
+Rectangle.prototype.isSquare = function(length, width){
+  if (this.length == this.width) {
+    return true
+  }
+  else {
+    return false
+  }
+}
+Rectangle.prototype.area = function(length, width){
+return (this.length*this.width)
+}
+
+Rectangle.prototype.perimeter = function(length, width){
+return (this.length + this.width)
+}
+
+var triangle1 = new Rectangle(4, 5)
+var triangle2 = new Rectangle(12, 9)
+var triangle3 = new Rectangle(6, 10)
+
+
 
 function Triangle(sideA, sideB, sideC) {
   this.sideA = sideA;
@@ -17,4 +38,3 @@ function LineSegment(x1, y1, x2, y2) {
   this.x2 = x2;
   this.y2 = y2;
 }
-a
