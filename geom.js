e3d3e7a3232efef804b0abbd46beb5ad405f3c97
Woo Yun <hh747hh@gmail.com>
2016-03-02 06:36:58 -0500

    completed before obtuse

--- geom.js
+++ geom.js
@@ -3,12 +3,59 @@ function Rectangle(length, width) {
   this.width = width;
 }
 
+Rectangle.prototype.isSquare = function(){
+  if(this.length == this.width) {
+  return true;
+  }
+  else {
+  return false;
+  }
+};
+
+Rectangle.prototype.area = function() {
+  return this.length * this.width;
+};
+
+Rectangle.prototype.perimeter = function() {
+  return this.length * 2 + this.width *2;
+};
+
 
 function Triangle(sideA, sideB, sideC) {
   this.sideA = sideA;
   this.sideB = sideB;
   this.sideC = sideC;
 }
+
+Triangle.prototype.isEquilateral = function() {
+  if(this.sideA == this.sideB && this.sideA == this.sideC){
+    return true;
+  }
+  else {
+    return flase;
+  };
+}
+
+Triangle.prototype.isIsosceles = function() {
+  if((this.sideA == this.sideB) && (this.sideA !== this.sideC) || (this.sideB == this.sideC) && (this.sideB == this.sideA) || (this.sideC == this.sideB) && (this.sideC !== this.sideA)){
+    return true;
+  }
+  else {
+    return false;
+  }
+};
+
+Triangle.prototype.area = function() {
+  var s = (this.sideA + this.sideB + this.sideC) / 2;
+  return Math.sqrt(s * ((s-this.sideA)+(s-this.sideB)+(s-this.sideC));
+};
+
+Triangle.prototype.obtuse = function() {
+  
+}
+
+
+
 
 
 function LineSegment(x1, y1, x2, y2) {
