1bc32c53f840b904c7045ecf31ae634e2e61271e
Sarah Ivey <sarahivey1@yahoo.com>
2016-03-03 05:57:46 -0500

    attempt to make prototype for rectangle

--- geom.js
+++ geom.js
@@ -2,7 +2,22 @@ function Rectangle(length, width) {
   this.length = length;
   this.width = width;
 }
-
+// augment its prototype with the following methods:
+//
+// * `isSquare` - returns true if the rectangle is a square.
+// * `area` - calculates the area of the rectangle.
+// * `perimeter` - calculates the perimeter of the rectangle.
+// Create a few rectangles with different lengths and widths.
+Rectangle.prototype.isSquare = function(){
+  return this.length == this.width;
+};
+Rectangle.prototype.area= function(){
+  return this.length * this.width;
+};
+Rectangle.prototype.perimeter= function(){
+  return this.length * 2 + this.width * 2;
+};
+  
 
 function Triangle(sideA, sideB, sideC) {
   this.sideA = sideA;
