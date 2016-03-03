25580280b9ace43addc2e36273b7dd2b22264c03
jblakeman <blakeman.john@gmail.com>
2016-03-02 21:39:51 -0500

    Initial working solution

--- geom.js
+++ geom.js
@@ -3,6 +3,16 @@ function Rectangle(length, width) {
   this.width = width;
 }
 
+Rectangle.prototype.isSquare = function() {
+    if (this.length === this.width) return true;
+    return false;
+};
+Rectangle.prototype.area = function() {
+    return this.length * this.width;
+};
+Rectangle.prototype.perimeter = function() {
+    return (this.length + this.width) * 2;
+};
 
 function Triangle(sideA, sideB, sideC) {
   this.sideA = sideA;
@@ -10,6 +20,22 @@ function Triangle(sideA, sideB, sideC) {
   this.sideC = sideC;
 }
 
+Triangle.prototype.isEquilateral = function() {
+    return (this.sideA === this.sideB && this.sideB === this.sideC);
+};
+Triangle.prototype.isIsosceles = function() {
+    return (this.sideA === this.sideB || this.sideA === this.sideC);
+};
+Triangle.prototype.area = function() {
+    var semiP = (this.sideA + this.sideB + this.sideC) / 2;
+    return Math.sqrt(semiP * (semiP - this.sideA) *
+                       (semiP - this.sideB) * (semiP * this.sideC));
+};
+Triangle.prototype.isObtuse = function() {
+    var abSquared = (this.sideA * this.sideA) + (this.sideB * this.sideB);
+    var cSquared = this.sideC * this.sideC;
+    return ((cSquared / 2) < abSquared && abSquared < cSquared);
+};
 
 function LineSegment(x1, y1, x2, y2) {
   this.x1 = x1;
@@ -17,3 +43,11 @@ function LineSegment(x1, y1, x2, y2) {
   this.x2 = x2;
   this.y2 = y2;
 }
+
+LineSegment.prototype.length = function() {
+    var xDelta = this.x2 - this.x1;
+    var yDelta = this.y2 - this.y1;
+    return Math.sqrt((xDelta * xDelta) + (yDelta * yDelta));
+};
+
+
