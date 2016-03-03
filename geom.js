1e36f56c6b688c7d99914c0b0e5783c52e7f2b7a
Lyndsey Newsome <lyndseysnewsome@gmail.com>
2016-03-02 21:06:45 -0500

    Made the prototype functions for triangle and rectangle.

--- geom.js
+++ geom.js
@@ -3,6 +3,14 @@ function Rectangle(length, width) {
   this.width = width;
 }
 
+Rectangle.prototype = {
+  isSquare: function(){
+    this.length===this.width{
+      return ("I'm a square")
+    }else if ("I'm not a square")
+  }
+}
+
 
 function Triangle(sideA, sideB, sideC) {
   this.sideA = sideA;
@@ -10,10 +18,41 @@ function Triangle(sideA, sideB, sideC) {
   this.sideC = sideC;
 }
 
+Triangle.prototype = {
+  isEquilateral: function () {
+    this.sideA === this.sideB
+    this.sideB === this.sideC {
+      return("I'm Equalateral!")
+    }
+  }
+  isIsosceles: function () {
+    this.sideA === this.sideB
+    this.sideB === this.sideC
+    this.sideC === this.sideA
+    {
+      return("I'm an Isosceles!")
+    }
+  }
+  isObtuse: function () {
+    this.sideA === this.sideB
+    this.sideB === this.sideC{
+      return("I'm Obtuse!")
+    }
+  }
+  area function () {
+    return ()
+
+  }
+
+}
 
 function LineSegment(x1, y1, x2, y2) {
   this.x1 = x1;
   this.y1 = y1;
   this.x2 = x2;
   this.y2 = y2;
+}
+
+LineSegment.prototype = {
+
 }
