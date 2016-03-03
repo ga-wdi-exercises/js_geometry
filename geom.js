c364a13fcbaed68ced52dd027d6129005785d768
YOUR FULL NAME <farishta.haider@gmail.com>
2016-03-02 23:41:32 -0500

    Need to test new variable

--- geom.js
+++ geom.js
@@ -4,19 +4,17 @@ function Rectangle(length, width) {
 }
 
 Rectangle.prototype = {
-isSquare: function(){
-  return this.length == this.width;
-},
-//returns true if the rectangle is a square.
-area: function(){
-  return this.length * this.width;
-},
-// calculates the area of the rectangle.
-perimeter: function(){
-  return(this.length + this.width)*2;
-}
-// calculates the perimeter of the rectangle.
+  isSquare: function(){
+    return this.length == this.width;
+  },
+  area: function(){
+    return this.length * this.width;
+  },
+  perimeter: function(){
+    return(this.length + this.width)*2;
+  }
 };
+
 function Triangle(sideA, sideB, sideC) {
   this.sideA = sideA;
   this.sideB = sideB;
@@ -26,22 +24,17 @@ Triangle.prototype = {
   isEquilateral: function{
     return this.sideA === this.sideB && this.sideB === this.sideC;
   },
-// isEquilateral - returns true if the triangle is equilateral.
-isIsosceles: function {
-  return this.sideA === this.sideB || this.sideB === this.sideC || this.sideC === this.sideA;
-},
-// isIsosceles - return true if the triangle is isosceles.
-area: function{
-  var semiperimeter = (this.sideA + this.sideB + this.sideC) / 2;
-  var area = Math.sqrt(s *(s-this.sideA) * (s - sideB) * (s - sideC));
-  return area;
-},
-// area - calculates the area of the Triangle.
-isObtuse: function{
-// ?Pythagorean Theorem?
-}
-
-// isObtuse - returns true if the triangle is obtuse.
+  isIsosceles: function {
+    return this.sideA === this.sideB || this.sideB === this.sideC || this.sideC === this.sideA;
+  },
+  area: function{
+    var semiperimeter = (this.sideA + this.sideB + this.sideC) / 2;
+    var area = Math.sqrt(s *(s-this.sideA) * (s - sideB) * (s - sideC));
+    return area;
+  },
+  isObtuse: function{
+    // isObtuse - returns true if the triangle is obtuse.
+  }
 }
 
 function LineSegment(x1, y1, x2, y2) {
@@ -57,6 +50,4 @@ LineSegment.prototype = {
     var b = thix.y1 + this.y2;
     return Math.sqrt(a * a)+(b * b)
   }
-}
-
-// length – calculates the length of the (x1, y1) --> (x2, y2) line segment.
+};
