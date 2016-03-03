7e8d42f4a8eab4e79132612cf395b2464ef6c78c
winishasmith <winishasmith@gmail.com>
2016-03-03 06:55:20 -0500

    added rectangle prototypes for square, area, and perimeter

--- geom.js
+++ geom.js
@@ -3,17 +3,34 @@ function Rectangle(length, width) {
   this.width = width;
 }
 
+Rectangle.prototype.isSquare = function(){
+  if (this.length === this.width){
+    return true;
+  }
+  else {
+    return false;
+  }
+}
 
-function Triangle(sideA, sideB, sideC) {
-  this.sideA = sideA;
-  this.sideB = sideB;
-  this.sideC = sideC;
+Rectangle.prototype.area = function(){
+  return this.length * this.width;
+}
+
+Rectangle.prototype.perimeter = function(){
+  return (this.length + this.width) * 2;
 }
 
 
-function LineSegment(x1, y1, x2, y2) {
-  this.x1 = x1;
-  this.y1 = y1;
-  this.x2 = x2;
-  this.y2 = y2;
-}
+// function Triangle(sideA, sideB, sideC) {
+//   this.sideA = sideA;
+//   this.sideB = sideB;
+//   this.sideC = sideC;
+// }
+
+//
+// function LineSegment(x1, y1, x2, y2) {
+//   this.x1 = x1;
+//   this.y1 = y1;
+//   this.x2 = x2;
+//   this.y2 = y2;
+// }
