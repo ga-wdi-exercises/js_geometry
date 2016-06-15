70116ec93069fbb66f283369cb5849c8ef621070
Andrew Young <prince.of.utah@gmail.com>
2016-06-15 14:34:08 -0400

    added psuedocode to line segment

--- geom.js
+++ geom.js
@@ -17,39 +17,39 @@ Recatangle.prototype = {
   }
 };
 
-function Triangle(triSideA, triSideb, triSideC) {
-  this.triSideA = triSideA;
-  this.triSideb = triSideb;
-  this.tritriSideC = triSideC;
+function Triangle(sideA, sideB, sideC) {
+  this.sideA = sideA;
+  this.sideB = sideB;
+  this.sideC = sideC;
 }
 Triangle.prototype = {
   // isEquilateral - returns true if the triangle is equilateral.
   isEquilateral: function(){
-    return this.triSideA === this.triSideb === this.triSideC;
+    return this.sideA === this.sideB === this.sideC;
   },
   // isIsosceles - return true if the triangle is isosceles.
-    // return - isIsosceles if triSideA is equal to triSideB but not equal to triSideC.
-      //or if triSideA is equal to triSideC but not equal to triSideB.
-        //or if triSideB is equal to triSideC but not equal to triSideA.
+    // return - isIsosceles if sideA is equal to sideB but not equal to sideC.
+      //or if sideA is equal to sideC but not equal to sideB.
+        //or if sideB is equal to sideC but not equal to sideA.
   isIsosceles: function(){
     return
-      this.triSideA === this.triSideB
-      this.triSideA === this.triSideC
-      this.triSideB === this.triSideC
+      this.sideA === this.sideB
+      this.sideA === this.sideC
+      this.sideB === this.sideC
       //need to figure out the relation.
   },
   // area - calculates the area of the Triangle.((b * h)/2)
   //Area	=	 √	 p	 (	p	−	a	) 	(	p	−	b	)	 (	p	−	c	)
   area: function(){
-    var p = (this.triSideA + this.triSideb + this.triSideC) / 2;
-    var area = Math.sqrt(p (p - this.triSideA) (p - this.triSideb) (p - this.triSideC))
+    var p = (this.sideA + this.sideB + this.sideC) / 2;
+    var area = Math.sqrt(p (p - this.sideA) (p - this.sideB) (p - this.sideC))
     return area;
   },
   // isObtuse - returns true if the triangle is obtuse.(has a =< 90' angle)
     // Must use Pythagorean theorem A^2 + B^2 = C^2
     // Could use Math.pow somehow to get the exponents
   isObtuse: function(){
-    return ((this.triSideA * this.sideA) + (this.triSideB * this.sideB)) =< (this.triSideC * this.sideC)
+    return ((this.sideA * this.sideA) + (this.sideB * this.sideB)) =< (this.sideC * this.sideC)
     //**Test this**
     //I'm not sure this is correct but in my head I don't see why not.
   }
@@ -62,6 +62,10 @@ function LineSegment(x1, y1, x2, y2) {
   this.x2 = x2;
   this.y2 = y2;
 }
+// Need to use the Pathagorean theorem for this as well.
+//  A^2 + B^2 = C^2
+//  ((x1 - y1)^2 + (x2 - y2)^2) = C^2
+// then just take the squar root of C^2
 
 //Augment its prototype with the following method:
 //  length – calculates the length of the (x1, y1) --> (x2, y2) line segment.
