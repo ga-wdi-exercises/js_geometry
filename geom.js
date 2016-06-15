fc69976fafb1580443960fd8eb065e0b87caac5e
Andrew Young <prince.of.utah@gmail.com>
2016-06-15 08:49:31 -0400

    added some triangle functions

--- geom.js
+++ geom.js
@@ -17,18 +17,30 @@ Recatangle.prototype{
   }
 }
 
-
 function Triangle(sideA, sideB, sideC) {
   this.sideA = sideA;
   this.sideB = sideB;
   this.sideC = sideC;
 }
+Triangle.prototype{
+  // isEquilateral - returns true if the triangle is equilateral.
+  isEquilateral: function(){
+    return this.SideA === this.sideB === this.sideC;
+  }
+  // isIsosceles - return true if the triangle is isosceles.
+  isIsosceles: function(){
+    return this.sideA === this.sideB
+  }
+  // area - calculates the area of the Triangle.
+  area: function(){
 
-//Augment its prototype with the following methods:
-// isEquilateral - returns true if the triangle is equilateral.
-// isIsosceles - return true if the triangle is isosceles.
-// area - calculates the area of the Triangle.
-//  isObtuse - returns true if the triangle is obtuse.
+  }
+  //  isObtuse - returns true if the triangle is obtuse.
+  isObtuse: function(){
+
+  }
+}
+
 
 function LineSegment(x1, y1, x2, y2) {
   this.x1 = x1;
