82a6007f6fe85a4c5a4b31a33bbc4e16f5915dd7
Arthur Adams <arthuradams@gmail.com>
2016-06-14 22:20:41 -0400

    added rectangle and triangle functions

--- geom.js
+++ geom.js
@@ -3,6 +3,9 @@ function Rectangle(length, width) {
   this.width = width;
 }
 
+Rectangle.prototype.isSquare = function() { return (this.length == this.width) };
+Rectangle.prototype.area = function() { return (this.length * this.width) };
+Rectangle.prototype.perimeter = function() { return ( 2 * this.length + 2 * this.width) };
 
 function Triangle(sideA, sideB, sideC) {
   this.sideA = sideA;
@@ -10,6 +13,32 @@ function Triangle(sideA, sideB, sideC) {
   this.sideC = sideC;
 }
 
+Triangle.prototype.isEquilateral = function() {
+  return ((this.sideA == this.sideB) &&
+          (this.sideA == this.sideC) &&
+          (this.sideB == this.sideC));
+}
+
+Triangle.prototype.isIsosceles = function() {
+  return ((this.sideA == this.sideB) ||
+          (this.sideA == this.sideC) ||
+          (this.sideB == this.sideC));
+}
+
+Triangle.prototype.area = function() {
+  var s = (this.sideA + this.sideB + this.sideC) / 2;
+  return Math.sqrt( s * (s - this.sideA) * (s - this.sideB) * (s - this.sideC) );
+}
+
+Triangle.prototype.isObtuse = function() {
+  function obtuseCheck(x,y,z) {
+    return (x * x + y * y < z * z);
+  }
+  return (obtuseCheck(this.sideA,this.sideB,this.sideC) ||
+          obtuseCheck(this.sideB,this.sideC,this.sideA) ||
+          obtuseCheck(this.sideC,this.sideA,this.sideB)
+  );         
+}
 
 function LineSegment(x1, y1, x2, y2) {
   this.x1 = x1;
