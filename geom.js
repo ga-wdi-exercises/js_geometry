1c6d01b8bafac6486521cdc6af617003f4ed3373
Andrew Young <prince.of.utah@gmail.com>
2016-06-14 22:01:16 -0400

    Initial commit and added psuedo code/instructions

--- geom.js
+++ geom.js
@@ -3,6 +3,11 @@ function Rectangle(length, width) {
   this.width = width;
 }
 
+//Augment its prototype with the following methods:
+//  isSquare - returns true if the rectangle is a square.
+//  area - calculates the area of the rectangle.
+//  perimeter - calculates the perimeter of the rectangle.
+
 
 function Triangle(sideA, sideB, sideC) {
   this.sideA = sideA;
@@ -10,6 +15,11 @@ function Triangle(sideA, sideB, sideC) {
   this.sideC = sideC;
 }
 
+//Augment its prototype with the following methods:
+// isEquilateral - returns true if the triangle is equilateral.
+// isIsosceles - return true if the triangle is isosceles.
+// area - calculates the area of the Triangle.
+//  isObtuse - returns true if the triangle is obtuse.
 
 function LineSegment(x1, y1, x2, y2) {
   this.x1 = x1;
@@ -17,3 +27,6 @@ function LineSegment(x1, y1, x2, y2) {
   this.x2 = x2;
   this.y2 = y2;
 }
+
+//Augment its prototype with the following method:
+//  length – calculates the length of the (x1, y1) --> (x2, y2) line segment.
