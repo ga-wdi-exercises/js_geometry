2410cb379f2ffc0da5542a0de9779ea284c16235
Chris Gardner <ctgardner89@gmail.com>
2016-06-19 13:59:02 -0400

    stopping poitn point *

--- geom.js
+++ geom.js
@@ -2,18 +2,32 @@ function Rectangle(length, width) {
   this.length = length;
   this.width = width;
 }
-
-
+Rectangle.prototype.isSquare =
+ function(){
+ if (this.length === this.width){
+ return true
+ }else{
+ return false
+ }
+}
+Rectangle.prototype.area =
+ function(){
+ return this.length * this.width
+ }
+Rectangle.prototype.perimeter =
+  function(){
+    return 2*(this.length + this.width)
+  }
 function Triangle(sideA, sideB, sideC) {
-  this.sideA = sideA;
-  this.sideB = sideB;
-  this.sideC = sideC;
+  this.sideA = sideA
+  this.sideB = sideB
+  this.sideC = sideC
 }
 
 
 function LineSegment(x1, y1, x2, y2) {
-  this.x1 = x1;
-  this.y1 = y1;
-  this.x2 = x2;
-  this.y2 = y2;
+  this.x1 = x1
+  this.y1 = y1
+  this.x2 = x2
+  this.y2 = y2
 }
