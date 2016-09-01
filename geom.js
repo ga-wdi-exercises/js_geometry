403b5d5e9cc37fb896ea29ad262fed588f1d1533
Nick Olds <nolds9@yahoo.com>
2016-09-01 14:30:12 -0400

    converts solution to use class syntax

--- geom.js
+++ geom.js
@@ -3,6 +3,18 @@ class Rectangle {
     this.length = length;
     this.width = width;
   }
+
+  isSquare() {
+    return this.length === this.width;
+  }
+
+  area() {
+    return this.length * this.width;
+  }
+
+  perimeter() {
+    return 2 * (this.length + this.width);
+  }
 }
 
 
@@ -11,6 +23,26 @@ class Triangle {
     this.sideA = sideA;
     this.sideB = sideB;
     this.sideC = sideC;
+  }
+
+  isEquilateral() {
+    return this.sideA === this.sideB && this.sideB === this.sideC;
+  }
+
+  isIsosceles() {
+    return this.sideA === this.sideB || this.sideA === this.sideC || this.sideB === this.sideC;
+  }
+
+  area() {
+    var p = (this.sideA + this.sideB + this.sideC) / 2;
+    var area = Math.sqrt(p * (p - this.sideA) * (p - this.sideB) * (p - this.sideC));
+    return area;
+  }
+
+  isObtuse() {
+    var sides = [this.sideA,this.sideB,this.sideC];
+    sides.sort();
+    return Math.pow(sides[0], 2) + Math.pow(sides[1], 2) < Math.pow(sides[2], 2);
   }
 }
 
@@ -21,5 +53,11 @@ class LineSegment {
     this.y1 = y1;
     this.x2 = x2;
     this.y2 = y2;
+  }
+
+  length() {
+    var a = this.x2 - this.x1;
+    var b = this.y2 - this.y1;
+    return Math.sqrt(a * a + b * b);
   }
 }
