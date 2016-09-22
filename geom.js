d5156d9954686ced8f0c04568e20c1916ba1b49c
Kevin Mahoney <kjmahoney13@gmail.com>
2016-09-22 18:48:22 -0400

    initial commit

--- geom.js
+++ geom.js
@@ -3,7 +3,25 @@ class Rectangle {
     this.length = length;
     this.width = width;
   }
+  isSquare(){
+    if (this.length === this.width){
+      return true
+    }else{
+      return false
+    }
+  }
+  area(){
+    let area = (this.length * this.width)
+    console.log(`The area of the rectangle is ${area}`)
+  }
+  perimeter(){
+    let perimeter = ((2*this.length) + (2 * this.width))
+    console.log(`The perimeter of the rectangle is ${perimeter}`)
+  }
+
 }
+
+const newRectangle = new Rectangle(6,3);
 
 
 class Triangle {
@@ -12,7 +30,23 @@ class Triangle {
     this.sideB = sideB;
     this.sideC = sideC;
   }
+  isEquilateral(){
+    if (sideA===sideB && sideA===sideC){
+      return true
+  }else{
+      return false
+    }
 }
+  isIsosceles(){
+    if (sideA===sideB || sideA ===sideC || sideB === sideC){
+      return true
+    }else{
+      return false
+    }
+  }
+
+
+
 
 
 class LineSegment {
@@ -25,8 +59,8 @@ class LineSegment {
 }
 
 // NOTE: DO NOT REMOVE OR ALTER
-module.exports = {
-  Rectangle: Rectangle,
-  Triangle: Triangle,
-  LineSegment: LineSegment
-}
+// module.exports = {
+//   Rectangle: Rectangle,
+//   Triangle: Triangle,
+//   LineSegment: LineSegment
+// }
