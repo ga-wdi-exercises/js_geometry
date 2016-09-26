045510982d192d35b68f94979550e1721e62626d
Domonique Griffin <griffind2009@gmail.com>
2016-09-26 14:04:11 -0400

    finished all equations

--- geom.js
+++ geom.js
@@ -10,17 +10,17 @@ class Rectangle {
     return this.width * this.length;
   }
   calcPerimeter () {
-    return (2)*(this.length * this.width);
+    return ((this.length + this.width) + (this.length + this.width) *2);
   }
 }
 const square = new Rectangle (4, 4)
-//console.log(square.isSquare);
+console.log(square.isSquare());
 
 const area = new Rectangle (5, 9)
-//console.log(area.calcArea);
+console.log(area.calcArea());
 
 const perimeter = new Rectangle (6, 7)
-//console.log(perimeter.calcPerimeter);
+console.log(perimeter.calcPerimeter());
 
 
 class Triangle {
@@ -36,24 +36,25 @@ class Triangle {
     return this.sideA === this.sideB || this.sideC === this.sideA || this.sideC === this.sideB;
   }
   getArea () {
-    return ((2 * this.sideA/this.sideB) * (this.sideB) *2)
+      return (((this.sideB /this.sideA) * 2) * (this.sideB))/2
   }
+
   isObtuse () {
     return this.sideA ** 2 + this.sideB ** 2 < this.sideC ** 2
   }
 }
 
 const equilateral = new Triangle (3, 4, 7)
-//console.log(equilateral.isEquilateral)
+console.log(equilateral.isEquilateral())
 
 const isoceles = new Triangle (5, 5, 9)
-//console.log(isoceles.isIsoceles);
+console.log(isoceles.isIsoceles());
 
-const areaT = new Triangle
-console.log(areaT.getArea)
+const areaT = new Triangle(4,5, 3)
+console.log(areaT.getArea())
 
 const obtuse = new Triangle (2, 1, 10)
-//console.log(obtuse.isObutse)
+console.log(obtuse.isObtuse())
 
 class LineSegment {
   constructor(x1, y1, x2, y2){
@@ -63,14 +64,13 @@ class LineSegment {
     this.y2 = y2;
   }
   calcLength () {
-    return (x1 + y1) && (x2 + y2);
+    return Math.sqrt(((this.x2-this.x1)**2)-((this.y2-this.y1)**2));
   }
 }
 
 const length = new LineSegment (2, 1, 6, 4)
-//console.log(length.calcLength)
+console.log(length.calcLength())
 
-//still need area of Triangle and length of line segment
 
 // NOTE: DO NOT REMOVE OR ALTER
 module.exports = {
