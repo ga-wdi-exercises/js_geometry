4b4e68d145a7c76e81fa8bce8034304cadfe581c
Michael Rubin <michael@rubinsworld.com>
2016-09-26 18:58:48 -0400

    homework completed

--- geom.js
+++ geom.js
@@ -1,3 +1,6 @@
+const PI=Math.PI;
+
+
 class Rectangle {
   constructor(length, width) {
     this.length = length;
@@ -14,6 +17,35 @@ class Rectangle {
   }/* parimeter */
 } /* class */
 
+class AdjacentSides {
+    constructor(longestSide,triangle) {
+      this.longestSide=longestSide;
+      this.adjacent=[];
+      this.triangle=triangle;
+  }   /* constructor */
+   getAdjSides(){
+    const sideA=this.triangle.sideA;
+    const sideB=this.triangle.sideB;
+    const sideC=this.triangle.sideC;
+
+    switch (this.longestSide) {
+      case sideA:
+           this.adjacent.push(sideB);
+           this.adjacent.push(sideC);
+          break;
+      case sideB:
+          this.adjacent.push(sideA);
+          this.adjacent.push(sideC);
+      break;
+      case sideC:
+          this.adjacent.push(sideA);
+          this.adjacent.push(sideB);
+        break;
+      default:
+        break;
+    } /* switch */
+  } /* method getAdjSides */
+} /* class AdjacentSides */
 
 class Triangle {
   constructor(sideA, sideB, sideC){
@@ -37,22 +69,24 @@ class Triangle {
 
 
   area() {
-    /*  - calculates the area of the Triangle. */
-/*    Area	=	 √	 p	 (	p	−	a	) 	(	p	−	b	)	 (	p	−	c	)
-where p is half the perimeter, or
-a	+	b	+	c
-2
-*/
-   let p=this.parameter()/2;
-   console.log(p)
-   return Math.sqrt(p*(p-this.sideA) * (p-this.sideB) * (p-this.sideC));
 
+  /********************************************************************
+  * - calculates the area of the Triangl using Heron's formula.
+  *             ___________________________________________
+  *    Area	=	 √	 p	 (	p	−	a	) 	(	p	−	b	)	 (	p	−	c	)
+  *        Where p is perimeter /2
+  * *******************************************************************/
+
+   let p=this.parameter()/2;  /* calculate 1/2 parameter */
+   return Math.sqrt(p*(p-this.sideA) * (p-this.sideB) * (p-this.sideC));
+  }
+  getLongestSegment() {
+  return Math.max(this.sideA,this.sideB,this.sideC);
   }
 
   isObtuse() {
     /* - returns true if the triangle is obtuse. */
-/*
-    obtuse angle definition. An angle that measures more than
+/*    obtuse angle definition. An angle that measures more than
     90 degrees but less than 180 degrees. (Compare acute angle
     and right angle.) */
 /*
@@ -61,43 +95,19 @@ use The Law of Cosines first to calculate one of the angles.
 then use The Law of Cosines again to find another angle.
 and finally use angles of a triangle add to 180° to find the last angle. */
 
-/* find the largest side */
- let side1=0;
- let side2=0;
- let side3=0;
 
-  if ( this.sideA > this.sideB) {
-      side1=this.sideA;
-  } else {
-     side1=this.sideB;
-  }
-   if (side1 < this.sideC) {
-    side1 = this.sideC
-   }
-
-  switch (side1) {
-    case this.sideA:
-        side2=this.sideB;
-        side3=this.sideC;
-        break;
-    case this.sideB:
-        side2=this.sideA;
-        side3=this.sideC;
-        break;
-    case this.sideC:
-        side2=this.sideA;
-        side3=this.sideB;
-      break;
-    default:
-      break;
-  } /* switch */
+ let side1=this.getLongestSegment(); /* find the largest side */
+ let adjSides= new AdjacentSides(side1,this); /* find adjecent sides */
+ adjSides.getAdjSides();
+ let side2=adjSides.adjacent[0];
+ let side3=adjSides.adjacent[1];
 
   console.log("side1=",side1,"side2=",side2,"side3=",side3);
   let cosB = ((side2*side2)+(side3*side3)-(side1*side1))/(2*side2*side3);
   console.log("cosB=",cosB)
   let radB=Math.acos(cosB);
   console.log('radB=',radB);
-  let degB=(180*radB)/3.14159;
+  let degB=(180*radB)/PI;
   console.log('degb=',degB);
   return ( degB >90);
 
@@ -127,6 +137,31 @@ class LineSegment {
     this.x2 = x2;
     this.y2 = y2;
   }
+  length () {
+    /*  length – calculates the length of the (x1, y1) --> (x2, y2) line segment. */
+    /********************************************************************
+    * - calculates the area of the Triangl using Heron's formula.
+    *               ___________________________________________
+    *    Area	=	 	 /              2                  2
+    *             √	 (	x2 −	x1)    + 	(	y2	−	y1	)
+    *
+    * *******************************************************************/
+        /* find difference between two points */
+      let x1=this.x1;
+      let x2=this.x2;
+      let y2=this.y2;
+      let y1=this.y1;
+       /* square differnce */
+      let xsq= (x2-x1) * (x2-x1);
+      let ysq= (y2-y1) * (y2-y1);
+      let ss= xsq+ysq;
+      return Math.sqrt(ss);  /* return sq root of difference */
+
+  } /* length method */
+
+
+
+
 }
 
 // NOTE: DO NOT REMOVE OR ALTER
@@ -149,3 +184,15 @@ const tri2= new Triangle( 11.62, 7.42,15.22);
 console.log ( "Obtuse", tri2.isObtuse());
 const tri3= new Triangle( 11.25, 6,8);
 console.log ( "Obtuse", tri3.isObtuse());
+const tri4= new Triangle( 3, 3,3);
+console.log ( "Obtuse", tri4.isObtuse());
+const tri5= new Triangle( 3, 4,5);
+console.log ( "Obtuse", tri5.isObtuse());
+const tri6= new Triangle( 7, 4,4);
+console.log ( "Obtuse", tri6.isObtuse());
+
+const ls2=new LineSegment(0,0,3,0);
+console.log("Line Segment 0,0,3,0 = ",ls2.length());
+
+const ls1=new LineSegment(1,1,4,5);
+console.log("Line Segment 1,1,4,5 = ",ls1.length());
