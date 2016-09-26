f60fbf5cd3d6819d16887b2549c2c307368b3d2e
Nick Oki <nick.oki@live.com>
2016-09-26 17:43:11 -0400

    Triangle Solution

--- geom.js
+++ geom.js
@@ -29,7 +29,38 @@ class Triangle {
 		this.sideB = sideB;
 		this.sideC = sideC;
 	}
+	isEquilateral() {
+		if (this.sideA === this.sideB && this.sideB === this.sideC && this.sideC === this.sideA) {
+			return true
+		} else {
+			return false
+		}
+	}
+	isIsosceles() {
+		if (this.sideA === this.sideB || this.sideB === this.sideC || this.sideC === this.sideA) {
+			return true
+		} else {
+			return false
+		}
+	}
+	area() {
+		let s = (this.sideA + this.sideB + this.sideC) / 2
+		return ((s * (s - this.sideA) * (s - this.sideB) * (s - this.sideC)) ** 0.5)
+	}
+	isObtuse() {
+		if (this.sideA ** 2 + this.sideB ** 2 < this.sideC ** 2 ||
+		    this.sideB ** 2 + this.sideC ** 2 < this.sideA ** 2 ||
+		    this.sideC ** 2 + this.sideA ** 2 < this.sideB ** 2) {
+			return true
+		} else {
+			return false
+		}
+	}
 }
+
+var tri1 = new Triangle(3, 3, 3)
+var tri2 = new Triangle(3, 4, 5)
+var tri3 = new Triangle(4, 7, 4)
 
 
 class LineSegment {
