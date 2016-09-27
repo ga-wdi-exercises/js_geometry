13aaabcb44ca144ac54b9f22f058e319ddc10119
William Keating <keating49@gmail.com>
2016-09-27 13:46:28 -0400

    isSquare, area, & perimeter

--- geom.js
+++ geom.js
@@ -3,30 +3,48 @@ class Rectangle {
     this.length = length;
     this.width = width;
   }
-}
 
+  isSquare() {
+    if (this.length === this.width) {
+      console.log("this is a square!")
+    } else {
+      console.log("this is not a square")
+    }
+  }
 
-class Triangle {
-  constructor(sideA, sideB, sideC){
-    this.sideA = sideA;
-    this.sideB = sideB;
-    this.sideC = sideC;
+  area() {
+    return this.length * this.width
+  }
+  // var test = new Rectangle()
+  perimeter(){
+    return ((this.length * 2) + (this.width * 2))
+
   }
 }
+// var test = new Rectangle(5, 5)
+// var test2 = new Rectangle(7, 5)
 
-
-class LineSegment {
-  constructor(x1, y1, x2, y2){
-    this.x1 = x1;
-    this.y1 = y1;
-    this.x2 = x2;
-    this.y2 = y2;
-  }
-}
-
-// NOTE: DO NOT REMOVE OR ALTER
-module.exports = {
-  Rectangle: Rectangle,
-  Triangle: Triangle,
-  LineSegment: LineSegment
-}
+// class Triangle {
+//   constructor(sideA, sideB, sideC){
+//     this.sideA = sideA;
+//     this.sideB = sideB;
+//     this.sideC = sideC;
+//   }
+// }
+//
+//
+// class LineSegment {
+//   constructor(x1, y1, x2, y2){
+//     this.x1 = x1;
+//     this.y1 = y1;
+//     this.x2 = x2;
+//     this.y2 = y2;
+//   }
+// }
+//
+// // NOTE: DO NOT REMOVE OR ALTER
+// module.exports = {
+//   Rectangle: Rectangle,
+//   Triangle: Triangle,
+//   LineSegment: LineSegment
+// }
