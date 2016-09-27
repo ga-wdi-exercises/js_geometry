952b5b527eb290c102aeb3837ade118ae308a901
Mike Flores <michaelprflores@gmail.com>
2016-09-27 14:30:59 -0400

    augmented triange with Isosceles and Equilateral methods is complete

--- geom.js
+++ geom.js
@@ -6,8 +6,10 @@ class Rectangle {
     isSquare(){
         if (this.length == this.width){
             console.log("This is a Square!")
+            return true
         } else {
             console.log("Not a square!")
+            return false
         } 
     }
     area(){
@@ -19,28 +21,45 @@ class Rectangle {
 }
 
 
-// test for square
-// var test = new Rectangle(5,5)
-// test.isSquare()
-
-
 class Triangle {
-  constructor(sideA, sideB, sideC){
-    this.sideA = sideA;
-    this.sideB = sideB;
-    this.sideC = sideC;
-  }
+    constructor(sideA, sideB, sideC){
+        this.sideA = sideA
+        this.sideB = sideB
+        this.sideC = sideC
+    }
+    isIsocsceles(){
+        if (
+                (this.sideA == this.sideB &&  this.sideA != this.sideC)
+            ||  (this.sideA == this.sideC &&  this.sideA != this.sideB)
+            ||  (this.sideB == this.sideC && this.sideB !=  this.sideA)
+        ) {
+            console.log("It's an Isocsceles!")
+            return true
+        }
+    }
+    isEquilateral(){
+        if (this.sideA == this.sideB && this.sideB == this.sideC) {
+            console.log("It's an Equilateral!")
+            return true
+        }
+    }
+    area(){
+    }
 }
 
 
-class LineSegment {
-  constructor(x1, y1, x2, y2){
-    this.x1 = x1;
-    this.y1 = y1;
-    this.x2 = x2;
-    this.y2 = y2;
-  }
-}
+var triangleOne = new Triangle(5,5,5)
+var triangleTwo = new Triangle(6,3,6)
+var triangleThree = new Triangle(1,2,3)
+
+// class LineSegment {
+//   constructor(x1, y1, x2, y2){
+//     this.x1 = x1;
+//     this.y1 = y1;
+//     this.x2 = x2;
+//     this.y2 = y2;
+//   }
+// }
 
 // NOTE: DO NOT REMOVE OR ALTER
 // module.exports = {
