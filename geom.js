aaa49cb5f5eb075c0c5a6f317e2e8861ea1bb60a
Ian Hollands <ihollands@outlook.com>
2016-09-27 20:21:52 -0400

    complete

--- geom.js
+++ geom.js
@@ -27,7 +27,38 @@ class Triangle {
     this.sideB = sideB;
     this.sideC = sideC;
   }
-}
+  isEquilateral() {
+    if (this.sideA === this.sideB && this.sideB === this.sideC) {
+      return true;
+    } else return false;
+  }
+  isIsosceles() {
+    if (this.sideA === this.sideB && this.sideB !== this.sideC) {
+      return true;
+    } else if (this.sideB === this.sideC && this.sideC !== this.sideA) {
+      return true;
+    } else if (this.sideC === this.sideA && this.sideA !== this.sideB) {
+      return true;
+    } else return false;
+    }
+    area() {
+      var s = (this.sideA+this.sideB+this.sideC)/2;
+      return Math.sqrt((s*(s-this.sideA)*(s-this.sideB)*(s-this.sideC)));
+    }
+    isObtuse() {
+      if (this.sideA**2 + this.sideB**2 < this.sideC**2) {
+        return true;
+      } else if (this.sideB**2 + this.sideC**2 < this.sideA**2) {
+        return true;
+      } else if (this.sideC**2 + this.sideA**2 < this.sideB**2) {
+        return true;
+      } else return false;
+    }
+  }
+
+var triangle1 = new Triangle(10,10,10);
+var triangle2 = new Triangle(4,4,7);
+var triangle3 = new Triangle(4,15,13);
 
 
 class LineSegment {
@@ -37,7 +68,17 @@ class LineSegment {
     this.x2 = x2;
     this.y2 = y2;
   }
+ length() {
+  return Math.sqrt((Math.abs((this.x2-this.x1)))**2 + (Math.abs((this.y2-this.y1)))**2)
+ }
 }
+
+var lineSegment1 = new LineSegment(2,3,3,5);
+var lineSegment2 = new LineSegment(2,-3,4,-8);
+var lineSegment3 = new LineSegment(-6,-5,2,8);
+var lineSegment4 = new LineSegment(-5,6,5,6);
+
+
 
 // NOTE: DO NOT REMOVE OR ALTER
 module.exports = {
