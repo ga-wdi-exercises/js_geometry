0cb8e4b5c3b5d6d7a6d846128d7eb1a2ed494828
Eric Christopher Green <VERDE611@GMAIL.COM>
2017-02-02 18:03:15 -0500

    mathed out

--- geom.js
+++ geom.js
@@ -3,8 +3,23 @@ class Rectangle {
     this.length = length;
     this.width = width;
   }
-}
 
+  isSquare() {
+    if (this.length == this.width){
+      return true
+    }else{
+      return false
+    }
+  }
+  area() {
+    return this.length*this.width
+  }
+  perimeter() {
+    return (this.length+this.width)*2
+  }
+
+}
+let square = new Rectangle(4,2)
 
 class Triangle {
   constructor(sideA, sideB, sideC){
@@ -12,8 +27,37 @@ class Triangle {
     this.sideB = sideB;
     this.sideC = sideC;
   }
+  isEquilateral() {
+    if (this.sideA==this.sideB && this.sideA==this.sideC){
+    return true
+  }else{
+    return false
+    }
+  }
+  isIsosceles() {
+    if (this.sideA==this.sideB || this.sideA==this.sideC || this.sideB==this.sideC){
+      return true
+    }else{
+      return false
+    }
+  }
+  area() {
+    var sP = (this.sideA+this.sideB+this.sideC)/2
+    var sA = sP-this.sideA
+    var sB = sP-this.sideB
+    var sC = sP-this.sideC
+    return Math.sqrt(sP*sA*sB*sC)
+  }
+  isObtuse() {
+    if (this.sideA !== this.sideB && this.sideA !== this.sideC && this.sideB !== this.sideC){
+      return true
+    }else{
+      return false
+    }
+  }
 }
 
+let triangle = new Triangle(15,16,17)
 
 class LineSegment {
   constructor(x1, y1, x2, y2){
@@ -22,7 +66,17 @@ class LineSegment {
     this.x2 = x2;
     this.y2 = y2;
   }
+  length(){
+    console.log(this)
+    let sX = this.x1-this.x2
+    let sY = this.y1-this.y2
+    let sXx = Math.pow(sX,2)
+    let sYy = Math.pow(sY,2)
+    return Math.sqrt(sXx+sYy)
+  }
 }
+
+let line = new LineSegment(7, 3, 8, 2)
 
 // NOTE: DO NOT REMOVE OR ALTER
 module.exports = {
