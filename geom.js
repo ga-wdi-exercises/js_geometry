64878ac6aa672516e2f75b1019a85b6c31e112ca
Nat Carter <n.goldman.carter@gmail.com>
2017-02-05 10:08:26 -0500

    Nat's initial commit

--- geom.js
+++ geom.js
@@ -3,8 +3,23 @@ class Rectangle {
     this.length = length;
     this.width = width;
   }
+  isSquare() {
+    if (this.length === this.width) {
+      return true;
+    }
+    return false;
+  }
+  area() {
+    return this.length * this.width;
+  }
+  perimeter() {
+    return 2 * (this.length + this.width);
+  }
 }
 
+let recA = new Rectangle(1, 1);
+let recB = new Rectangle(2, 5);
+let recC = new Rectangle(4, 7);
 
 class Triangle {
   constructor(sideA, sideB, sideC){
@@ -12,8 +27,23 @@ class Triangle {
     this.sideB = sideB;
     this.sideC = sideC;
   }
+  isEquilateral() {
+    if (this.sideA === this.sideB && this.sideB === this.sideC) {
+      return true;
+    }
+    return false;
+  }
+  isIsosceles() {
+    if (this.sideA === this.sideB || this.sideB === this.sideC || this.sideA === this.sideC) {
+      return true;
+    }
+    return false;
+  }
+  area() {
+    let s = (this.sideA + this.sideB + this.sideC) / 2;
+    return Math.sqrt(s*(s-sideA)*(s-sideB)*(s-sideC));
+  }
 }
-
 
 class LineSegment {
   constructor(x1, y1, x2, y2){
@@ -21,6 +51,9 @@ class LineSegment {
     this.y1 = y1;
     this.x2 = x2;
     this.y2 = y2;
+  }
+  length() {
+    return sqrt(Math.abs(x2 - x1) + Math.abs(y2 - y1));
   }
 }
 
