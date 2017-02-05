07d905a81a7ebd3a9d8ada925e458fcbf05db1a0
Hannah Schuster <hschuster9@gmail.com>
2017-02-05 15:00:20 -0500

    triangle

--- geom.js
+++ geom.js
@@ -24,8 +24,27 @@ class Triangle {
     this.sideB = sideB;
     this.sideC = sideC;
   }
-}
+  isEquilateral(){
+    return this.sideA === this.sideB && this.sideB === this.sideC;
+  }
+  isIsosceles(){
+    return this.sideA === this.sideB || this.sideA === this.sideC || this.sideB === this.sideC;
 
+  }
+  area(){
+    var perimeter = (this.sideA + this.sideB + this.sideC) /2;
+    var area = Math.sqrt(perimeter*(perimeter-this.sideA)*(perimeter-this.sideB)*(perimeter-this.sideC));
+      return area;
+  }
+  isObtuse(){
+    var side = [this.sideA, this.sideB, this.sideC];
+    return Math.pow(side[0], 2) + Math.pow(side[1],2) < Math.pow(side[2], 2);
+  }
+
+}
+let triangle1 = new Triangle(1,1,3);
+let triangle2 = new Triangle(2,2,2);
+let triangle3 = new Triangle(5,6,7)
 
 class LineSegment {
   constructor(x1, y1, x2, y2){
