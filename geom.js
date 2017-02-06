bc03458b7f81999d9854d989b547cf20b02ae314
Chris Wightman <wightman_christopher@bah.com>
2017-02-06 14:30:39 -0500

    Completed Geometry Assignment

--- geom.js
+++ geom.js
@@ -3,14 +3,43 @@ class Rectangle {
     this.length = length;
     this.width = width;
   }
+
+  isSquare(){
+    return this.length = this.width
+  }
+
+  area(){
+    return this.length * this.width
+  }
+
+  perimeter(){
+    return ((this.length*2) + (this.width*2))
+  }
+
 }
 
+
+var rectangle1 = new Rectangle(16,22)
+var rectangle2 = new Rectangle(33,19)
+var rectangle3 = new Rectangle(4,21)
 
 class Triangle {
   constructor(sideA, sideB, sideC){
     this.sideA = sideA;
     this.sideB = sideB;
     this.sideC = sideC;
+  }
+
+  isEquilateral(){
+    return (this.sideA = this.sideB) && (this.sideB = this.sideC)
+  }
+
+  isIsosceles(){
+    return this.sideA = (this.sideB || this.sideC) 
+  }
+
+  isObtuse(){
+    return (Math.pow(this.sideA, 2) + Math.pow(this.sideB, 2)) < (Math.pow(this.sideC, 2))
   }
 }
 
@@ -21,6 +50,10 @@ class LineSegment {
     this.y1 = y1;
     this.x2 = x2;
     this.y2 = y2;
+  }
+
+  length(){
+    return Math.sqrt(Math.pow(x1 - x2, 2) + Math.pow(y1 - y2,2))
   }
 }
 
