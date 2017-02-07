9a90935b93842b198b55253fd9990f7e7e2efb76
ISAAC BENJAMIN DUARTE <Isaac.b.duarte@icloud.com>
2017-02-07 16:32:46 -0500

    done

--- geom.js
+++ geom.js
@@ -3,6 +3,15 @@ class Rectangle {
     this.length = length;
     this.width = width;
   }
+  isSqare() {
+    return (this.length == this.width)? true:false
+  }
+  perimeter() {
+    return (this.length*2)+(this.width*2)
+  }
+  area() {
+    return this.length * this.width
+  }
 }
 
 
@@ -11,6 +20,29 @@ class Triangle {
     this.sideA = sideA;
     this.sideB = sideB;
     this.sideC = sideC;
+  }
+  isEquilateral() {
+    return (this.sideA == this.sideB)&&(this.sideB == this.sideC)&&(this.sideA == this.sideC)? true:false
+  }
+  isIsosceles() {
+    let a = this.sideA
+    let b = this.sideB
+    let c = this.sideC
+    return ((a==b)&&(a!=c))||((a==c)&&(a!=b))||((b==c)&&(b!=a))? true:false
+  }
+  area() {
+    let a = this.sideA
+    let b = this.sideB
+    let c = this.sideC
+    let s = (a+b+c)/2
+    return Math.sqrt(s*(s-a)*(s-b)*(s-c))
+  }
+  isObtuse() {
+    let a = this.sideA
+    let b = this.sideB
+    let c = this.sideC
+    return (Math.pow(a,2) + Math.pow(b,2) < Math.pow(c,2))||(Math.pow(a,2) + Math.pow(c,2) < Math.pow(b,2))
+    ||(Math.pow(c,2) + Math.pow(b,2) < Math.pow(a,2))? true:false
   }
 }
 
@@ -21,6 +53,12 @@ class LineSegment {
     this.y1 = y1;
     this.x2 = x2;
     this.y2 = y2;
+  }
+  length() {
+    let x = Math.abs(this.x1-this.x2)
+    let y = Math.abs(this.y1-this.y2)
+    let z = Math.pow(x,2)+Math.pow(y,2)
+    return Math.sqrt(z)
   }
 }
 
