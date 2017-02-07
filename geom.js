b99999960349290358dd3d59fdc548084915f8de
Nyketha Lyken <Nyketha.Lyken@gmail.com>
2017-02-07 08:40:28 -0500

    fixed returns

--- geom.js
+++ geom.js
@@ -4,22 +4,26 @@ class Rectangle {
     this.width = width;
   }
   isSquare() {
-    x = this.length === this.width;
+     if(this.length === this.width){
     console.log("square");
   }
+}
   area() {
-    x = this.length * this.width;
-    console.log("rectangle area");
+    if (this.length * this.width) {
+      console.log("rectangle area");
+    }
   }
   perimeter() {
-    x = (this.length + this.width) * 2;
-    console.log("all the sides");
+    if ((this.length + this.width) * 2) {
+      console.log("all the sides");
+    }
   }
 }
 
 let Square = new Rectangle("4cm", "4cm");
 let Rectangle1 = new Rectangle("6cm", "10cm");
 let Rectangle2 = new Rectangle("12cm", "6cm");
+
 
 
 class Triangle {
@@ -29,25 +33,32 @@ class Triangle {
     this.sideC = sideC;
   }
   isEquilateral() {
-    x = this.sideA === this.sideB === this.sideC;
-    console.log("we equal");
+    if (this.sideA === this.sideB === this.sideC) {
+      console.log("we equal");
+    }
   }
   isIsosceles() {
-    x = this.sideA === this.sideB;
-    console.log("semi-equal");
+    if (this.sideA === this.sideB) {
+      console.log("semi-equal");
+    }
   }
   area() {
-    x = (this.sideC * this.sideB)/2;
-    console.log("area aight");
+    if ((this.sideC * this.sideB)/2) {
+      console.log("area aight");
+    }
   }
   isObtuse() {
-    x = (this.sideB > this.sideA > this.sideC);
-    console.log("obtuse");
+    if (this.sideB > this.sideA > this.sideC) {
+      console.log("obtuse");
+    }
   }
 }
+
 let equal = new Triangle("7cm", "7cm", "7cm");
 let semi = new Triangle("9cm", "9cm", "5cm");
 let obtuse = new Triangle("20cm", "12cm", "7cm");
+
+
 
 class LineSegment {
   constructor(x1, y1, x2, y2){
@@ -57,12 +68,16 @@ class LineSegment {
     this.y2 = y2;
   }
   length() {
-    z = Math.pow(this.x1, 2) + Math.pow(this.y1, 2);
-      console.log("length1");
-    w = Math.pow(this.x2, 2) + Math.pow(this.y2, 2);
+     if (Math.pow(this.x1, 2) + Math.pow(this.y1, 2)) {
+       console.log("length1");
+     }
+    if (Math.pow(this.x2, 2) + Math.pow(this.y2, 2)) {
       console.log("length2");
+    }
   }
 }
+
+
 
 // NOTE: DO NOT REMOVE OR ALTER
 module.exports = {
