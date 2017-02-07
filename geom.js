d6b55f4e3175de847e2b57cec4fe760c78c37476
Bedig Galladian <bgalladian@gmail.com>
2017-02-06 20:22:55 -0500

    created the triangles

--- geom.js
+++ geom.js
@@ -25,7 +25,7 @@ perimiter(){
   return this.perimeter = 2 *(this.length + this.width)
 }
 
-let
+
 
 class Triangle {
   constructor(sideA, sideB, sideC){
@@ -33,6 +33,29 @@ class Triangle {
     this.sideB = sideB;
     this.sideC = sideC;
   }
+}
+
+let triangle1 = new Trianlge (1, 1, 3)
+let triangle2 = new Triangle (2, 2, 2)
+let trianlge3 = new Triangle (5, 6, 7)
+
+isEquilateral(){
+  return this.sideA === this.sideB && this.sideb === this.sideC;
+}
+
+isIsosceles(){
+  return this.sideA === this.sideB || this.sideB === this.sideC || this.sideA === this.sideC;
+}
+
+isObtuse(){
+  var side = [this.sideA, this.sideB, this.sideC]
+  return Math.pow(side[0], 2) + Math.pow(side[1], 2) < Math.pow(side[2], 2);
+}
+
+area(){
+    var perimeter = (this.sideA + this.sideB + this.sideC) /2;
+    var area = Math.sqrt(perimeter*(perimeter-this.sideA)*(perimeter-this.sideB)*(perimeter-this.sideC));
+      return area;
 }
 
 
@@ -46,6 +69,8 @@ class LineSegment {
     this.y2 = y2;
   }
 }
+
+
 
 // NOTE: DO NOT REMOVE OR ALTER
 module.exports = {
