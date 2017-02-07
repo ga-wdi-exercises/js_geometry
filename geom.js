4f6538d2da9b72a35a7c95b6d9d4f7afa1c1af3f
Dan Lin <dlin2102@gmail.com>
2017-02-06 22:08:41 -0500

    final

--- geom.js
+++ geom.js
@@ -2,9 +2,27 @@ class Rectangle {
   constructor(length, width) {
     this.length = length;
     this.width = width;
+    }
+  isSquare(){
+    if(this.length == this.width){
+    return "true";
+      }
+    else{
+      return "false";
+      }
+    }
+  area(){
+    var areaOfRectangle = this.length * this.width
+    return areaOfRectangle;
+  }
+  perimeter(){
+    var perimeterOfRectangle = (this.length * 2) + (this.width * 2)
+    return perimeterOfRectangle;
   }
 }
 
+const square1 = new Rectangle(5,5);
+const square2 = new Rectangle(3,7);
 
 class Triangle {
   constructor(sideA, sideB, sideC){
@@ -12,8 +30,30 @@ class Triangle {
     this.sideB = sideB;
     this.sideC = sideC;
   }
+  isEquilateral(){
+    if(sideA === sideB && sideA === sideC){
+    return "true";
+  }
+  }
+  isIsosceles(){
+    if(this.sideA == this.sideB || this.sideB == this.sideC){
+      return "true";
+    }
+  }
+  isObtuse(){
+    if(sideA === sideB && sideA === sideC){
+      return "false";
+    }
+    else if(this.sideA == this.sideB || this.sideB == this.sideC){
+      return "false";
+    }
+    else{
+      return "true";
+    }
+  }
 }
 
+cons triangle1 = new Triangle(3,2,4)
 
 class LineSegment {
   constructor(x1, y1, x2, y2){
@@ -21,6 +61,10 @@ class LineSegment {
     this.y1 = y1;
     this.x2 = x2;
     this.y2 = y2;
+  }
+  length(){
+    var calculateLength = ((y2 - y1) * (y2 - y1)) + ((x2 - x1) * (x2 - x1)) * 2;
+    return calculateLength;
   }
 }
 
