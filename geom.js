311c729c8a8ce2607c8a36facf39bf3215a8851e
Caroline Chen <carolinecaroline@gmail.com>
2017-03-30 17:09:25 -0400

    delete previous answers

--- geom.js
+++ geom.js
@@ -1,30 +1,3 @@
-class Rectangle {
-  constructor(length, width) {
-    this.length = length;
-    this.width = width;
-  }
-}
-
-
-class Triangle {
-  constructor(sideA, sideB, sideC){
-    this.sideA = sideA;
-    this.sideB = sideB;
-    this.sideC = sideC;
-  }
-}
-
-
-class LineSegment {
-  constructor(x1, y1, x2, y2){
-    this.x1 = x1;
-    this.y1 = y1;
-    this.x2 = x2;
-    this.y2 = y2;
-  }
-}
-
-// NOTE: DO NOT REMOVE OR ALTER
 module.exports = {
   Rectangle: Rectangle,
   Triangle: Triangle,
