05f6d65bf10adf81996d459b430a594d73314355
Caroline Chen <carolinecaroline@gmail.com>
2017-03-30 19:07:33 -0400

    augment method for LineSegment

--- geom.js
+++ geom.js
@@ -9,17 +9,32 @@ class Rectangle {
   area(){
     return (this.length * this.width);
   }
-  perimeter() {
+  perimeter(){
     return (2*this.length + 2*this.width);
   }
 }
 
 
-class Triangle {
+class Triangle{
   constructor(sideA, sideB, sideC){
     this.sideA = sideA;
     this.sideB = sideB;
     this.sideC = sideC;
+  }
+  isEquilateral(){
+    return((this.sideA == this.sideB) && (this.sideA == this.sideC));
+  }
+  //I'm considering an isosceles triangle as one having at least two even sides
+  isIsosceles(){
+    return((this.sideA == this.sideB)|| (this.sideA == this.sideC) || (this.sideB == this.sideC));
+  }
+  area() {
+    var s = ((this.sideA + this.sideB + this.sideC) / 2);
+    return(Math.sqrt(s * (s - this.sideA) * (s - this.sideB) * (s - this.sideC)));
+  }
+  isObtuse() {
+    //this assumes sideA is the longest side.  need to figure out how to determine longest side and adjust accordingly
+    return((Math.pow(this.sideA, 2)) > (Math.pow(this.sideB, 2) + Math.pow(this.sideC, 2)));
   }
 }
 
@@ -30,6 +45,9 @@ class LineSegment {
     this.y1 = y1;
     this.x2 = x2;
     this.y2 = y2;
+  }
+  length() {
+     return (Math.sqrt(Math.pow((this.x2-this.x1),2)+Math.pow((this.y2-this.y1),2)))
   }
 }
 
