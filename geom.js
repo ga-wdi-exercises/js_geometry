a4312dbec31da93530279266f7c720ade88e1991
Jonathan Wright <jonwright.va@gmail.com>
2017-03-30 17:47:27 -0400

    Completed homework

--- geom.js
+++ geom.js
@@ -3,6 +3,20 @@ class Rectangle {
     this.length = length;
     this.width = width;
   }
+  isSquare() {
+    if (this.length === this.width) {
+      console.log("true");
+      return true
+    } else {
+      console.log("false");
+    }
+  }
+  area() {
+    return this.length * this.width
+  }
+  perimeter() {
+    return (this.length * 2) + (this.width * 2)
+  }
 }
 
 
@@ -11,6 +25,35 @@ class Triangle {
     this.sideA = sideA;
     this.sideB = sideB;
     this.sideC = sideC;
+  }
+  isEquilateral() {
+    if (this.sideA === this.sideB && this.sideB === this.sideC) {
+      console.log("true");
+      return true
+    } else {
+      console.log("false");
+    }
+  }
+  isIsosceles() {
+    if(this.a === this.b && this.b !== this.c || this.b === this.c && this.c !== this.a || this.c === this.a && this.a !== this.c ) {
+      console.log('true');
+      return true
+    } else {
+      console.log();
+    }
+  }
+  area() {
+    let s = (this.sideA + this.sideB + this.sideC) / 2
+    let area = Math.sqrt(s(s - this.sideA)(s - this.sideB)(s - this.sideC));
+    return area;
+  }
+  isObtuse() {
+    if (this.sideA !== this.sideB && this.sideA !== this.sideC && this.sideB !== this.sideC) {
+      console.log('true');
+      return true
+    } else {
+      console.log('false');
+    }
   }
 }
 
@@ -22,7 +65,18 @@ class LineSegment {
     this.x2 = x2;
     this.y2 = y2;
   }
+  length() {
+    return Math.sqrt(Math.pow((this.x1 - this.x2), 2) + Math.pow((this.y1 - this.y2), 2))
+  }
 }
+
+let rect = new Rectangle(24, 60);
+let tri = new Triangle(34, 34, 34);
+let tri2 = new Triangle(34, 4, 4);
+let tri3 = new Triangle(6, 3, 4);
+let line = new LineSegment(1, 1, 5, 4);
+
+
 
 // NOTE: DO NOT REMOVE OR ALTER
 module.exports = {
