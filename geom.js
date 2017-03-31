d0ec6a58adab931f9cb14e9141b85c0fba46f37e
Andres Arango <andresarangov17@gmail.com>
2017-03-30 22:44:26 -0400

    finish geometry

--- geom.js
+++ geom.js
@@ -3,6 +3,17 @@ class Rectangle {
     this.length = length;
     this.width = width;
   }
+  isSquare() {
+    if (this.length === this.width) {
+      return true;
+    } else return false;
+  }
+  area() {
+    return this.length * this.width;
+  }
+  perimeter() {
+    return 2 * (this.length + this.width);
+  }
 }
 
 
@@ -11,6 +22,24 @@ class Triangle {
     this.sideA = sideA;
     this.sideB = sideB;
     this.sideC = sideC;
+  }
+  isEquilateral() {
+    return (this.sideA === this.sideB && this.sideB === this.sideC);
+  }
+  isIsosceles() {
+    return (this.sideA === this.sideB) || (this.sideA === this.sideC) || (this.sideB === this.sideC);
+  }
+  area() {
+    let p = (this.sideA + this.sideB + this.sideC) / 2;
+    let a = Math.sqrt(p * (p - this.sideA) * (p - this.sideB) * (p - this.sideC));
+    return a;
+  }
+  isObtuse() {
+    if (this.sideA > this.sideB && this.sideA > this.sideC) {
+      return Math.pow(this.sideA, 2) > Math.pow(this.sideB, 2) + Math.pow(this.sideC, 2);
+    } else if (this.sideB > this.sideC) {
+      return Math.pow(this.sideB, 2) > Math.pow(this.sideA, 2) + Math.pow(this.sideC, 2);
+    } else return Math.pow(this.sideC, 2) > Math.pow(this.sideA, 2) + Math.pow(this.sideB, 2);
   }
 }
 
@@ -22,7 +51,11 @@ class LineSegment {
     this.x2 = x2;
     this.y2 = y2;
   }
+  length() {
+    return Math.sqrt((this.x2 - this.x1) * (this.x2 - this.x1) + (this.y2 - this.y1) * (this.y2 - this.y1));
+  }
 }
+
 
 // NOTE: DO NOT REMOVE OR ALTER
 module.exports = {
