7e49330af7db5941b2d0caaef99e50efdd3836cb
Curtis Ostle <curtisostle@gmail.com>
2017-03-31 13:29:12 -0400

    finsihed geometry

--- geom.js
+++ geom.js
@@ -3,8 +3,21 @@ class Rectangle {
     this.length = length;
     this.width = width;
   }
+    isSquare() {
+      if (this.length === this.width) {
+        return true;
+      }
+      else {
+        return false;
+      }
+    }
+    area() {
+      return this.length * this.width;
+    }
+    perimeter() {
+      return (this.length * 2) + (this.width * 2);
+    }
 }
-
 
 class Triangle {
   constructor(sideA, sideB, sideC){
@@ -12,6 +25,32 @@ class Triangle {
     this.sideB = sideB;
     this.sideC = sideC;
   }
+    isEquilateral() {
+        if(this.sideA === this.sideB && this.sideA === this.sideC) {
+          return true;
+        }
+        else {
+            return false;
+          }
+    isIsosceles() {
+      if(this.sideA === this.sibeB || this.sideA === this.sideC || this.sideB === this.sideC){
+        return true;
+      }
+      else {
+        return false;
+      }
+    }
+    area() {
+      return this.sideA * this.sideB * this.sideC;
+    }
+    isObtuse() {
+      if(this.sideA !== this.sideB && this.sideA !== this.sideC && this.sideB !== this.sideC){
+        return true;
+      }
+      else {
+        return false;
+      }
+    }
 }
 
 
@@ -22,7 +61,12 @@ class LineSegment {
     this.x2 = x2;
     this.y2 = y2;
   }
+    length() {
+      return Math.sqrt((Math.pow(Math.abs(this.x1-this.y1),2)) + (Math.pow(Math.abs(this.x2-this.y2),2)));
+    }
 }
+
+
 
 // NOTE: DO NOT REMOVE OR ALTER
 module.exports = {
