8040cfed1e08b50523a98bc710dfc7d58ca47a3c
Luisa Gonzalez <THE_EMAIL_YOU_USE_FOR_GITHUB@EMAIL.COM>
2017-03-31 18:09:09 -0400

    geometry final

--- geom.js
+++ geom.js
@@ -1,10 +1,57 @@
 class Rectangle {
-  constructor(length, width) {
+  constructor(length, width){
     this.length = length;
     this.width = width;
   }
+
+isSquare(){
+  return (this.length===this.width)
+  }
+
+
+area(){
+  return (this.length*this.width)
 }
 
+
+perimeter(){
+  return 2 * (this.length+this.height)
+}
+
+}
+var square = new Rectangle(10,10);
+console.log(square);
+console.log(square.isSquare());
+console.log(class Rectangle {
+  constructor(length, width){
+    this.length = length;
+    this.width = width;
+  }
+
+isSquare(){
+  return (this.length===this.width)
+  }
+
+
+area(){
+  return (this.length*this.width)
+}
+
+
+perimeter(){
+  return 2 * (this.length+this.height)
+}
+
+
+var square = new Rectangle(10,10){
+console.log(square)
+console.log(square.isSquare())
+console.log(square.area);
+console.log(square.perimeter);
+
+
+
+}
 
 class Triangle {
   constructor(sideA, sideB, sideC){
@@ -13,6 +60,31 @@ class Triangle {
     this.sideC = sideC;
   }
 }
+isEquilateral(){
+  if(this.sideA===this.sideB&&this.sideB====this.sideC){
+      return true;
+  }
+}
+  isIsosceles(){
+this.sideA===this.sideC||
+this.sideB===this.sideC){
+     return true;
+  }
+}
+isArea(){
+  (this.sideA * this.sideB * this.sideC)
+  }
+
+  isObtuse(){
+    if(this.sideA !== this.sideB && this.sideA !==this.sideC && this.sideB !==this.sideC){
+      return true;
+    }
+  }
+}
+
+var triangle = new triangle(30,30,45)
+console.log(triangle)
+console.log(triangle.isIsosceles())
 
 
 class LineSegment {
@@ -23,10 +95,17 @@ class LineSegment {
     this.y2 = y2;
   }
 }
-
+length(){
+  return (Math.sqrt(Math.pow((this.x2-this.x1),2)+Math.pow((this.y2-this.y1),2)))
+ }
+}
 // NOTE: DO NOT REMOVE OR ALTER
 module.exports = {
   Rectangle: Rectangle,
   Triangle: Triangle,
   LineSegment: LineSegment
 }
+
+var distance = new LineSegment(4,2,8,2);
+console.log (distance.length());
+console.log (distance);
