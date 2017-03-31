a9461f7188172b05e189ab403e859c067fb46765
Caroline Chen <carolinecaroline@gmail.com>
2017-03-31 14:31:15 -0400

    correct isObtuse function

--- geom.js
+++ geom.js
@@ -34,7 +34,28 @@ class Triangle{
   }
   isObtuse() {
     //this assumes sideA is the longest side.  need to figure out how to determine longest side and adjust accordingly
-    return((Math.pow(this.sideA, 2)) > (Math.pow(this.sideB, 2) + Math.pow(this.sideC, 2)));
+    // return((Math.pow(this.sideA, 2)) > (Math.pow(this.sideB, 2) + Math.pow(this.sideC, 2)));
+
+    //square each number and put into an array
+    //find the max number and set that to a variable denoting longest side
+    //pop that out of the array and the rest will be the shorter sides, on the other side of the equation
+    var sidesArray = [];
+    var sideAsquared = Math.pow(this.sideA, 2);
+    var sideBsquared = Math.pow(this.sideB, 2);
+    var sideCsquared = Math.pow(this.sideC, 2);
+    // console.log(sideAsquared, sideBsquared, sideCsquared);
+    sidesArray.push(sideAsquared, sideBsquared, sideCsquared);
+    // console.log(sidesArray);
+    var longestSide = Math.max(sidesArray[0], sidesArray[1], sidesArray[2]);
+    // console.log(longestSide);
+    var indexLongestSide = sidesArray.indexOf(longestSide);
+    // console.log("index is " + indexLongestSide);
+    sidesArray.splice(indexLongestSide, 1);
+    // console.log(sidesArray);
+    var shortside1 = sidesArray[0];
+    var shortside2 = sidesArray[1];
+
+    return(longestSide > shortside1 + shortside2);
   }
 }
 
