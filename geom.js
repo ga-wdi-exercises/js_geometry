37f8d3529c7586f7213400c18a9a7d419a1e0c1a
Curry Lee <ej20curry@gmail.com>
2017-05-12 15:08:55 -0400

    done

--- geom.js
+++ geom.js
@@ -3,6 +3,20 @@ class Rectangle {
     this.length = length;
     this.width = width;
   }
+  isSquare(){
+    if (this.length === this.width)
+      return true;
+      else {
+        return false;
+      }
+  }
+  areaCalc(){
+    return this.length*this.width;
+  }
+  perimeter(){
+    var perim = (this.length*2)+(this.width*2);
+    return perim;
+  }
 }
 
 
@@ -12,6 +26,20 @@ class Triangle {
     this.sideB = sideB;
     this.sideC = sideC;
   }
+  isEquilateral(){
+    if (this.sideA === this.sideB && this.sideB === this.sideC){
+      return true;
+    }
+    else {
+      return false;
+    }
+  }
+    area(){
+      var p = (this.sideA + this.sideB + this.sideC)/2;
+      var area = Math.sqrt(p*((p-this.sideA)*(p-this.sideB)*(p-this.sideC)));
+      return area;
+    }
+
 }
 
 
@@ -21,8 +49,16 @@ class LineSegment {
     this.y1 = y1;
     this.x2 = x2;
     this.y2 = y2;
+
+  }
+  length(){
+    var base1 = this.x2-this.x1;
+    var base2 = this.y2-this.y1;
+    var distance = Math.sqrt((Math.pow(base1, 2)+ Math.pow(base2, 2)));
+    return distance;
   }
 }
+
 
 // NOTE: DO NOT REMOVE OR ALTER
 module.exports = {
