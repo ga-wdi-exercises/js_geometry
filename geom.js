4e18604e4f12055580bf26181ccdd2b2aa8a2741
Mark Moeller <MarkMoellerUVA@gmail.com>
2017-05-12 11:24:23 -0400

    Functional shapes

--- geom.js
+++ geom.js
@@ -3,6 +3,22 @@ class Rectangle {
     this.length = length;
     this.width = width;
   }
+
+  isSquare(){
+    if (this.length == this.width) {
+      return true
+    }else {
+      return false
+    }
+  }
+
+  area(){
+    return this.length * this.width
+  }
+
+  perimeter(){
+    return (this.length * 2) + (this.width * 2)
+  }
 }
 
 
@@ -12,6 +28,38 @@ class Triangle {
     this.sideB = sideB;
     this.sideC = sideC;
   }
+
+  isEquilateral(){
+    if (this.sideA == this.sideB && this.sideA == this.sideC) {
+      return true
+    }else {
+      return false
+    }
+  }
+
+  isIsosceles(){
+    if (this.sideA == this.sideB || this.sideA == this.sideC || this.sideB == this.sideC) {
+      return true
+    }else{
+      return false
+    }
+  }
+
+  isObtuse(){
+    if (Math.pow(this.sideA,2) + Math.pow(this.sideB,2) > Math.pow(this.sideC,2) \
+      || Math.pow(this.sideA,2) + Math.pow(this.sideC,2) > Math.pow(this.sideB,2) \
+      || Math.pow(this.sideB,2) + Math.pow(this.sideC,2) > Math.pow(this.sideA,2)) {
+        return true
+    }else {
+      return false
+    }
+  }
+
+  area(){
+    return .25 * (Math.sqrt((Math.pow((Math.pow(this.sideA,2) + Math.pow(this.sideB,2) + Math.pow(this.sideC,2)),2))\
+          - (2 *(Math.pow(this.sideA,4) + Math.pow(this.sideB,4) + Math.pow(this.sideC,4)))))
+  }
+
 }
 
 
@@ -22,7 +70,14 @@ class LineSegment {
     this.x2 = x2;
     this.y2 = y2;
   }
+
+  length(){
+    return Math.sqrt(Math.pow(this.x1 - this.x2,2) + Math.pow(this.y1 - this.y2,2))
+  }
 }
+
+var line = new LineSegment(0,0,5,0)
+var triangle = new Triangle(5,6,7)
 
 // NOTE: DO NOT REMOVE OR ALTER
 module.exports = {
