c50d50235a91ff8c6762f05a9cf19ab4c2a25def
Perry Fustero <pjfust@gmail.com>
2017-07-06 14:43:22 -0400

    finish homework

--- geom.js
+++ geom.js
@@ -1,26 +1,73 @@
 class Rectangle {
-  constructor(length, width) {
-    this.length = length;
-    this.width = width;
+  constructor (length, width) {
+    this.length = length
+    this.width = width
+  }
+  isSquare () {
+    if (this.length === this.width) {
+      return true
+    } else {
+      return false
+    }
+  }
+  area () {
+    return this.length * this.width
+  }
+  perimeter () {
+    return this.length * 2 + this.width * 2
   }
 }
-
 
 class Triangle {
-  constructor(sideA, sideB, sideC){
-    this.sideA = sideA;
-    this.sideB = sideB;
-    this.sideC = sideC;
+  constructor (sideA, sideB, sideC) {
+    this.sideA = sideA
+    this.sideB = sideB
+    this.sideC = sideC
+  }
+  isEquilateral () {
+    if (this.sideA === this.sideB && this.sideA === this.sideC) {
+      return true
+    } else {
+      return false
+    }
+  }
+  isIsosceles () {
+    if (this.sideA === this.sideB || this.sideA === this.sideC || this.sideB === this.sideC) {
+      return true
+    } else {
+      return false
+    }
+  }
+  area () {
+    let s = (this.sideA + this.sideB + this.sideC) / 2
+    let a = Math.sqrt(s * (s - this.sideA) * (s - this.sideB) * (s - this.sideC))
+    return a
+  }
+  perimeter () {
+    return (this.sideA + this.sideB + this.sideC)
+  }
+  obtuse () {
+    let sideArray = [this.sideA, this.sideB, this.sideC]
+    sideArray.sort(function (a, b) { return a - b })
+    if ((this.sideC * this.sideC) > (this.sideA * this.sideA) + (this.sideB * this.sideB))
+    return true
+    else {
+      return false
+    }
   }
 }
 
-
 class LineSegment {
-  constructor(x1, y1, x2, y2){
-    this.x1 = x1;
-    this.y1 = y1;
-    this.x2 = x2;
-    this.y2 = y2;
+  constructor (x1, y1, x2, y2) {
+    this.x1 = x1
+    this.y1 = y1
+    this.x2 = x2
+    this.y2 = y2
+  }
+  length () {
+    let xdiff = Math.abs(this.x2 - this.x1)
+    let ydiff = Math.abs(this.y2 - this.y1)
+    return Math.sqrt((xdiff * xdiff) + (ydiff * ydiff))
   }
 }
 
