9fa5316a7e07fa49e4f93bbf24b28bc7aa32d3ee
Colleen O'Reilly <colleenaoreilly@gmail.com>
2017-07-09 19:20:54 -0400

    geometry calculations added

--- geom.js
+++ geom.js
@@ -3,6 +3,18 @@ class Rectangle {
     this.length = length;
     this.width = width;
   }
+  isSquare () {
+    if (this.length === this.width){
+      return true
+    }
+    else return false
+  }
+  area () {
+    return this.length * this.width
+  }
+  perimeter () {
+    return (this.length * 2) + (this.width * 2)
+  }
 }
 
 
@@ -11,6 +23,33 @@ class Triangle {
     this.sideA = sideA;
     this.sideB = sideB;
     this.sideC = sideC;
+  }
+  isEquilateral () {
+    if (this.sideA === this.sideB === this.sideC){
+      return true
+    }
+    else return false
+  }
+  isIsosceles () {
+    if (this.sideA === this.sideB || this.sideA === this.sideC || this.sideB === this.sideC){
+      return true
+    }
+    else return false
+  }
+  area () {
+    return (1/4 * Math.sqrt((this.sideA + this.sideB + this.sideC) * (-this.sideA + this.sideB + this.sideC) * (this.sideA - this.sideB + this.sideC) * (this.sideA + this.sideB - this.sideC)))
+  }
+
+  isObtuse () {
+    let angle1 = (Math.acos(((this.sideB * this.sideB) + (this.sideC * this.sideC) - (this.sideA * this.sideA)) / (2 * this.sideB * this.sideC))) * 57.2958 //radians to degrees conversion
+    let angle2 = (Math.acos(((this.sideA * this.sideA) + (this.sideC * this.sideC) - (this.sideB * this.sideB)) / (2 * this.sideA * this.sideC))) * 57.2958 //radians to degrees conversion
+    let angle3 = 180 - angle1 - angle2
+    if (angle1 > 90 || angle2 > 90 || angle3 > 90) {
+      return 'true'
+    }
+    else {
+      return 'false'
+    }
   }
 }
 
@@ -21,6 +60,11 @@ class LineSegment {
     this.y1 = y1;
     this.x2 = x2;
     this.y2 = y2;
+  }
+  length () {
+  let xdiff = Math.abs (this.x1 - this.x2)
+  let ydiff = Math.abs (this.y1 - this.y2)
+  return Math.sqrt((xdiff * xdiff) / (ydiff * ydiff))
   }
 }
 
