ea22c311299d23f2b8482c80511cd3a7fb37cb5d
Richard Schram <rcschram@yahoo.com>
2017-07-10 08:58:13 -0400

    this is sparta

--- geom.js
+++ geom.js
@@ -1,32 +1,66 @@
 class Rectangle {
-  constructor(length, width) {
-    this.length = length;
-    this.width = width;
+  constructor (length, width) {
+    this.length = length
+    this.width = width
   }
+  isSquare () {
+    if (this.length == this.width) {
+      return true
+    } else {
+      return false
+    }
+  }
+  perimeter () {
+    return (this.length + this.width) * 2
+  }
+  area () {
+    return (this.length * this.width)
+  }
+
 }
-
-
 class Triangle {
-  constructor(sideA, sideB, sideC){
-    this.sideA = sideA;
-    this.sideB = sideB;
-    this.sideC = sideC;
+  constructor (sideA, sideB, sideC) {
+    this.sideA = sideA
+    this.sideB = sideB
+    this.sideC = sideC
   }
+  area () {
+    var perim = (this.sideA + this.sideB + this.sideC) / 2
+    return Math.sqrt(this.perim * (this.perim - this.sideA) * (this.perim - this.sideB) * (this.perim - this.sideC))
 }
-
-
-class LineSegment {
-  constructor(x1, y1, x2, y2){
-    this.x1 = x1;
-    this.y1 = y1;
-    this.x2 = x2;
-    this.y2 = y2;
+  isIsosceles () {
+    if ((this.sideA === this.sideB) || (this.sideA === this.sideB) || (this.sideB === this.sideC)) {
+      return true
+    } else {
+      return false
+    }
+  isEquilateral () {
+      if ((this.sideA === this.sideB) && (this.sideA === this.sideC)) {
+        return true
+      } else {
+        return false
+      }
   }
-}
+  isObtuse () {
+    temp = 0
+    x = this.sideA
+    y = this.sideB
+    z = this.sideC
+    if (x < y) {
+      temp = z
+      z = y
+      y = temp
+    }
+    if (z < x) {
+      temp = z
+      z = x
+      x = temp
+    }
 
-// NOTE: DO NOT REMOVE OR ALTER
-module.exports = {
-  Rectangle: Rectangle,
-  Triangle: Triangle,
-  LineSegment: LineSegment
+    if ((z * z > x * x + y * y)) {
+      return true
+    } else {
+      return false
+    }
+  }
 }
